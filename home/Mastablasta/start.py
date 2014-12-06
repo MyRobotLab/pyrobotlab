34ed9a887bfb092e89817c2d4256d127c0c1905f
theMastablasta <mastablasta@gmx.com>
2014-12-06 14:23:56 -0200

    Update start.py

--- home/Mastablasta/start.py
+++ home/Mastablasta/start.py
@@ -1,23 +1,13 @@
 from java.lang import String
 
-def heard(data):
-  print "Sphinx Data:", data
-
 botname = Runtime.createAndStart("botname", "ProgramAB")
 botname.startSession("ProgramAB", "default", "botname")
-sphinx = runtime.createAndStart("i01.ear","Sphinx")
-pats = botname.listPatterns("botname")
-sphinx_grammar = "|".join(pats)
-sphinx.startListening(sphinx_grammar)
 htmlfilter = Runtime.createAndStart("htmlfilter", "HtmlFilter")
 mouth = Runtime.createAndStart("i01.mouth", "Speech")
 mouth.setGoogleURI("http://thehackettfamily.org/Voice_api/api2.php?voice=Ryan&txt=")
-sphinx.addTextListener(botname)
-sphinx.addListener("publishText", python.name, "heard", String().getClass());
 botname.addTextListener(htmlfilter)
 htmlfilter.addTextListener(mouth)
 holygrail = Runtime.createAndStart("holygrail", "WebGUI")
-sphinx.attach(mouth)
 
 def randommove():
   i01.moveHand("right",50,50,50,60,50,50)
