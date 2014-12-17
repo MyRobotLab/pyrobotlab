525b63ecbf25d4c00ccdf6043ea86e775cd03c0a
GroG <supertick@gmail.com>
2014-12-17 13:53:32 -0800

    Update readInput.py

--- home/Markus/readInput.py
+++ home/Markus/readInput.py
@@ -7,6 +7,9 @@ arduino.connect("COM4")
 Pin0 = 14
 Pin1 = 15
 
+pin0 = None
+pin1 = None
+
 Ana0 = 10
 
 arduino.analogReadPollingStart(Pin0)
@@ -24,15 +27,20 @@ def publishPin(pin):
 
   if (pin.pin == 14):
     print("pin14.value ", pin.value)
+    pin0 = pin
     if (pin.value >= 500):
       mouth.speak("pin 0")
       sleep(2)
     
   elif (pin.pin == 15):
     print("pin15.value ", pin.value)
+    pin1 = pin
     if (pin.value >= 500):
       mouth.speak("pin 1")
       sleep(2)
+  
+  if (pin0.value >= pin1.value ):
+    print("pin0 is greater than pin1")
 
 
 #  print pin.pin, pin.value, pin.type, pin.source,
