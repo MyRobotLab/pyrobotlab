de7078b6b67fbb96d87b6ab8a2a95a8786eebb84
LunDev <LunDev@users.noreply.github.com>
2014-12-31 18:21:39 +0100

    Create AndVoiRec-Speech.py

---
+++ home/Ma.-Vo./AndVoiRec-Speech.py
@@ -0,0 +1,16 @@
+#AndroidVoiceRecognition combined with Speech
+from java.lang import String
+
+#creating services
+avr = Runtime.start("avr", "AndroidVoiceReconition")
+speech = Runtime.start("speech", "Speech")
+
+#recognition-def
+def heard(data):
+ print data
+ speech.speakBlocking(data);
+ #avr.sendToClient(data);
+ avr.startRecognition();
+
+#adding listener
+avr.addListener("recognized", python.name, "heard", String().getClass());
