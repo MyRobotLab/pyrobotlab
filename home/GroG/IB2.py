8542028d2278699559d316889b53fa9a4674139e
GroG <supertick@gmail.com>
2015-01-07 05:31:21 -0800

    checking in the latest InMoov bowler

--- home/GroG/IB2.py
+++ home/GroG/IB2.py
@@ -39,18 +39,7 @@ blender.attach(arduino)
 head = i01.startHead(leftPort)
 leftArm = i01.startLeftArm(leftPort)
 
-# get service handles to some of the servos
-neck = Runtime.getService("i01.head.neck")
-rothead = Runtime.getService("i01.head.rothead")
-
-#########  start mods ###################
-neck = Runtime.getService("i01.head.neck")
-neck.map(0,180,90,270)
-neck.setMinMax(-360, 360)
-
-
 ## get reference handles to services to directly modify them
-
 # left arm parts
 leftBicep = Runtime.getService("i01.leftArm.bicep")
 leftRotate = Runtime.getService("i01.leftArm.rotate")
@@ -62,26 +51,35 @@ jaw = Runtime.getService("i01.head.jaw")
 rothead = Runtime.getService("i01.head.rothead")
 neck = Runtime.getService("i01.head.neck")
 
-leftBicep.sweep(0,180)
-leftRotate.sweep(0,180)
-leftShoulder.sweep(0,180)
-leftOmoplate.sweep(0,180)
+#########  start mods ###################
+neck = Runtime.getService("i01.head.neck")
+neck.map(0,180,90,270)
+neck.setMinMax(-360, 360)
 
-jaw.sweep(0,180)
-rothead.sweep(0,180)
-neck.sweep(0,180)
+leftBicep.map(0,180,0,180)
+leftBicep.setMinMax(-360, 360)
 
-sleep(1)
-
-leftBicep.stop()
-leftRotate.stop()
-leftShoulder.stop()
-leftOmoplate.stop()
-
-jaw.stop()
-rothead.stop()
-neck.stop()
+jaw.setMinMax(0, 25)
 
 
-neck.moveTo(90)
-sleep(10)
+def sweep():
+  global leftBicep, leftRotate, leftShoulder, leftOmoplate, jaw, rothead, neck
+  leftBicep.sweep(0,180)
+  leftRotate.sweep(0,180)
+  leftShoulder.sweep(0,180)
+  leftOmoplate.sweep(0,180)
+
+  jaw.sweep(0,180)
+  rothead.sweep(0,180)
+  neck.sweep(0,180)
+
+def stop():
+  global leftBicep, leftRotate, leftShoulder, leftOmoplate, jaw, rothead, neck
+  leftBicep.stop()
+  leftRotate.stop()
+  leftShoulder.stop()
+  leftOmoplate.stop()
+
+  jaw.stop()
+  rothead.stop()
+  neck.stop()
