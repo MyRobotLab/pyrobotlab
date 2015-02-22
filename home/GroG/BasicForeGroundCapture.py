646948c0305778cf3a8656a954744acdd7610404
GroG <supertick@gmail.com>
2015-02-21 22:28:51 -0800

    Update BasicForeGroundCapture.py

--- home/GroG/BasicForeGroundCapture.py
+++ home/GroG/BasicForeGroundCapture.py
@@ -1,20 +1,29 @@
+
+def onOpenCVData(data):
+  if data != None:
+    boxes = data.getBoundingBoxArray()
+    if boxes != None:
+      print "found", boxes.size(), " boxes"
+
+      
 opencv = Runtime.start("uv","OpenCV")
 opencv.capture()
 
+python.subscribe("uv","publishOpenCVData", "onOpenCVData")
+ 
 opencv.addFilter("Detector")
 opencv.setDisplayFilter("Detector")
 detector = opencv.getFilter("Detector") 
-
+ 
 opencv.addFilter("FindContours")
 contours = opencv.getFilter("FindContours") 
-
-detector.learn()
-
-sleep(4) 
-
-detector.search()
-
-sleep(20)
-
-opencv.stopCapture()
  
+detector.learn()
+ 
+sleep(4) 
+ 
+detector.search()
+ 
+sleep(20)
+ 
+opencv.stopCapture()
