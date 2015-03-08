ab18df846cc431f0b009c1ef02edb0546e382c71
theMastablasta <mastablasta@gmx.com>
2015-03-08 12:44:17 -0300

    Update motor_poti.py

--- home/Mastablasta/dc_motor/motor_poti.py
+++ home/Mastablasta/dc_motor/motor_poti.py
@@ -1,19 +1,22 @@
 arduino = Runtime.createAndStart("arduino", "Arduino")
-arduino.connect("COM6") 
+arduino.connect("COM6")
+
 m1 = Runtime.createAndStart("m1","Motor")
-arduino.motorAttach("m1", 5, 6)
+arduino.motorAttach("m1", "TYPE_LPWM_RPWM", 5, 6)
+
 readAnalogPin = 0
 arduino.setSampleRate(9600)
 arduino.addListener("publishPin", "python", "publishPin")
 arduino.analogReadPollingStart(readAnalogPin)
+
 def publishPin(pin):
   print pin.pin, pin.value
-  if (pin.value >= 1020):
+  if (pin.value >= 1018):
      print ("right")
-     m1.move(-0.7)
+     m1.move(-0.5)
   if (pin.value <= 965):
      print ("left")
-     m1.move(0.3)
-  if (pin.value >= 965) and (pin.value <= 1020):
+     m1.move(0.5)
+  if (pin.value >= 965) and (pin.value <= 1018):
      print ("stop")
-     m1.move(-0.01)
+     m1.stop()
