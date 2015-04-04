f740c9533a6bb223600a83a8c57511220443e29f
GroG <supertick@gmail.com>
2015-04-04 06:35:23 -0700

    Create BasicMove.py

---
+++ home/GroG/BasicMove.py
@@ -0,0 +1,14 @@
+import bge # blender game engine
+import bpy  # blender python interface
+import math
+import mathutils
+from math import *
+
+
+x = 0
+def read_serial():
+    global x
+    x = x + 1
+    ob = bge.logic.getCurrentController().owner
+    ob.channels["jaw2"].joint_rotation = mathutils.Vector([radians(x/8),0,0])
+    print(x)
