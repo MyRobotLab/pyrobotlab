cb3fcf87b402333d96ce0d309d8d412303603eca
Kevin Watters <kwatters@kmwllc.com>
2015-09-19 14:32:33 -0400

    Update LeapMotion2.py

--- service/LeapMotion2.py
+++ service/LeapMotion2.py
@@ -14,7 +14,7 @@
 from com.leapmotion.leap.Finger import Type
 # from __future__ import division
 
-leap = Runtime.createAndStart("leap","LeapMotion2")
+leap = Runtime.createAndStart("leap","LeapMotion")
 
 leap.addFrameListener(python)
 
