8bcca6f154b3106fa1acb4288eb7b233ee242058
brotherbrown831 <brotherbrown831@yahoo.com>
2015-12-02 10:38:28 -0600

    Update MinimumJoystick.py

--- home/brotherbrown831/MinimumJoystick.py
+++ home/brotherbrown831/MinimumJoystick.py
@@ -1,14 +1,19 @@
 from org.myrobotlab.service import Joystick
+from org.myrobotlab.service import Arduino
 from org.myrobotlab.service import Runtime
 from time import sleep
 #---------------------------------Create Services----------------------
+arduino = Runtime.createAndStart("arduino","Arduino")
 joystick = Runtime.createAndStart("joystick","Joystick")
+motorleft = Runtime.start("motorleft","Motor")
 
 #----------------------Connect Peripherals-----------------------------------
 joystick.setController(0); #PC only - Pi needs new
 joystick.addInputListener(python)
 # Tell the joystick to turn on
 joystick.startPolling()
+arduino.connect("/dev/ttyACM0");
+arduino.motorAttach("motorleft", 5, 4) 
 
 #----------------------Define callback function for Joystick-----------
 def onJoystickInput(data):
@@ -20,7 +25,7 @@ def onJoystickInput(data):
   if (data.id == 'ry'):
     ryValue = float(data.value) 
   
+#-----------------------Main Loop----------------------------------------
 def print "the value of ry is" (ryValue)# this number could easily be used in other speed control functions
-''' 
-example use
+
 def motorLeft.move(255*(ryValue):
