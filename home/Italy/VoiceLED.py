c904cf4eb278bbba44adfad7782bb9225195e0f6
Alessandro Didonna <alessandro.didonna@gmail.com>
2015-12-08 01:41:33 +0100

    Update VoiceLED.py

--- home/Italy/VoiceLED.py
+++ home/Italy/VoiceLED.py
@@ -1,7 +1,10 @@
 python = Runtime.start("python","Python")
+speech = Runtime.start("speech","GoogleSpeech")
 webkitspeechrecognition = Runtime.start("webkitspeechrecognition","WebkitSpeechRecognition")
 arduino = Runtime.start("arduino","Arduino")
 arduino.connect("COM3")
+
+speech.setGoogleURI("http://thehackettfamily.org/Voice_api/api2.php?voice=Vittorio&txt=")
 
 
 def onText(data):
@@ -12,3 +15,5 @@ def onText(data):
          arduino.digitalWrite(13,0)
     
 webkitspeechrecognition.addListener("publishText","python","onText")
+
+speech.speak("ciao italia")
