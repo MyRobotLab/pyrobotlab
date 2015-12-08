f1a3fc1c3a07f74765e8e5374d254ed08a9161b3
Alessandro Didonna <alessandro.didonna@gmail.com>
2015-12-08 01:42:33 +0100

    Update VoiceLED.py

--- home/Italy/VoiceLED.py
+++ home/Italy/VoiceLED.py
@@ -6,14 +6,16 @@ arduino.connect("COM3")
 
 speech.setGoogleURI("http://thehackettfamily.org/Voice_api/api2.php?voice=Vittorio&txt=")
 
+specch.speak("italiano")
+
 
 def onText(data):
      print data
      if (data == "accendi luce"):
          arduino.digitalWrite(13,1)
+         speech.speak("luce accesa")
      elif (data == "spegni luce"):
          arduino.digitalWrite(13,0)
+         speech.speak("luce spenta")
     
 webkitspeechrecognition.addListener("publishText","python","onText")
-
-speech.speak("ciao italia")
