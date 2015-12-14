38f0a1499944acb53746433d42594ce023474dc6
Alessandro Didonna <alessandro.didonna@gmail.com>
2015-12-14 16:06:57 +0100

    Create VRAndroidPhoneandMyo.py

---
+++ home/Alessandruino/VRAndroidPhoneandMyo.py
@@ -0,0 +1,33 @@
+arduino = Runtime.createAndStart("arduino","Arduino")
+arduino.serial.refresh()
+sleep(2)
+arduino.connect("/dev/ttyACM0")
+
+remote = Runtime.start("remote","RemoteAdapter")
+neck = Runtime.createAndStart("neck","Servo")
+rothead = Runtime.createAndStart("rothead", "Servo")
+
+sleep(1)
+
+neck.attach(arduino,12)
+rothead.attach(arduino,13)
+
+neck.setMinMax(10,170)
+rothead.setMinMax(10,170)
+
+neck.setInverted(True)
+rothead.setInverted(True)
+
+oculus = Runtime.start("oculus","OculusDIY")
+
+def onOculusData(data):
+
+  neck.moveTo(int(data.pitch))
+  rothead.moveTo(int(data.yaw))
+
+def onMyoData(myodata):
+  if (myodata.currentPose == WAVE_OUT):
+    
+
+oculus.addOculusDataListener(python)
+remote.startListening()
