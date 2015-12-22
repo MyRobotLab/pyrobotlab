fa0014b99655b589ccef218de2536911340d4d43
theMastablasta <mastablasta@gmx.com>
2015-12-22 17:43:59 +0100

    Update startshort.py

--- home/Mastablasta/startshort.py
+++ home/Mastablasta/startshort.py
@@ -1,12 +1,11 @@
 import random
 from java.lang import String
+holygrail = Runtime.createAndStart("holygrail", "WebGui")
+wksr = Runtime.createAndStart("webkitspeechrecognition", "WebkitSpeechRecognition")
 alice2 = Runtime.createAndStart("alice2", "ProgramAB")
-alice2.startSession("ProgramAB", "default", "alice2")
+alice2.startSession("ProgramAB", "User", "alice2")
 htmlfilter = Runtime.createAndStart("htmlfilter", "HtmlFilter")
 mouth = Runtime.createAndStart("i01.mouth", "MarySpeech")
+wksr.addTextListener(alice2)
 alice2.addTextListener(htmlfilter)
 htmlfilter.addTextListener(mouth)
-holygrail = Runtime.create("holygrail", "WebGui")
-holygrail.startService()
-sleep(4)
-resp = alice2.getResponse("HI")
