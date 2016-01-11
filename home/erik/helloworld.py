02027c93e43f459bb86484568069f2e9884f69cf
Alessandro Didonna <alessandro.didonna@gmail.com>
2016-01-11 21:08:16 +0100

    Update helloworld.py

--- home/erik/helloworld.py
+++ home/erik/helloworld.py
@@ -1,7 +1,12 @@
 arduino = Runtime.start("arduino","Arduino")
 servo1 = Runtime.start("servo1","Servo")
+arduino.serial.refresh()
+sleep (1)
 
 arduino.serial.connect("COM3")
+
+sleep(1)
+
 servo1.attach(arduino,13)
 
 servo1.moveTo(20)
