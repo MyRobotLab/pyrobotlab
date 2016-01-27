b882f725989f9425130b4b5aa833636f091533a8
theMastablasta <mastablasta@gmx.com>
2016-01-27 07:33:52 +0100

    Update acapela_voice.py

--- home/Mastablasta/Python/acapela_voice.py
+++ home/Mastablasta/Python/acapela_voice.py
@@ -5,7 +5,23 @@ voices = acapelaSpeech.getVoices()
 for voice in voices:
     acapelaSpeech.setVoice("Ryan") 
 
+def CTG():
+    for voice in voices:
+        acapelaSpeech.setVoice("Klaus")
+
+def CTE():
+    for voice in voices:
+        acapelaSpeech.setVoice("Graham")
+
+def CTI():
+    for voice in voices:
+        acapelaSpeech.setVoice("Vittorio")
+
 #in this case "Ryan" is the voice
 #go to:
 #http://www.acapela-group.com/voices/repertoire/
 #here you find all voices available
+################################################
+# def CTG(): changes the voice to german
+# def CTE(): to english
+# def CTI(): to italian
