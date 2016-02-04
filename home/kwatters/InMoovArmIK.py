44f952badd0a5f6c9d5ac31b6dce2ea7aa497487
Kevin Watters <kwatters@kmwllc.com>
2016-02-03 19:52:17 -0500

    updating InMoovArmIK example python script.

--- home/kwatters/InMoovArmIK.py
+++ home/kwatters/InMoovArmIK.py
@@ -3,22 +3,56 @@
 # InverseKinematics for the InMoovArm
 # 
 ##########################################
+from time import sleep
 
+
+######################
+# work around until the race conditions in MRL are fixed.
+######################
+def resetArm():
+    global leftArm
+    leftArm.detach()
+    sleep(2)
+    leftArm.attach(leftPort)
+    sleep(2)
+    leftArm.bicep.attach()
+    leftArm.rotate.attach()
+    leftArm.shoulder.attach()
+    leftArm.omoplate.attach()
+
+############################################################
+# MAIN Entry Point
+############################################################
 # Create the IK Service
 ik = Runtime.createAndStart("ik", "InverseKinematics3D")
 # Create the left arm of the inmoov
 leftArm = Runtime.start("leftArm", "InMoovArm");
-
 # connect the left arm.
-leftPort = "COM31"
+leftPort = "COM21"
 leftArm.connect(leftPort)
+# update the IK service with the DH parameter description of the inmoov arm.
 ik.setCurrentArm(leftArm.getDHRobotArm())
+# wire in the callbacks between IK and the InMoovArm
 ik.addListener("publishJointAngles", leftArm.getName(), "onJointAngles")
 
-x = 0.0
-y = 0.0
-z = 0.0
+# start up a joystick service.
+joystick = Runtime.start("joystick", "Joystick");
+joystick.setController(2);
+joystick.addInputListener(ik);
+joystick.startPolling()
+# TODO: start polling.
 
-ik.moveTo(x,y,z)
+# start the web gui up.
+webgui = Runtime.createAndStart("webgui", "WebGui")
+# work around to re-attach the arduino and servos
+# sometimes they don't attach on first try due to some race conditions. 
 
-print(ik.currentPosition())
+
+
+resetArm()
+#x = 0.1
+#y = 0.1
+#z = 100.0
+#ik.moveTo(x,y,z)
+# print(ik.currentPosition())
+
