d96efb0d8c6b11bd856e13d126736d4da359dd90
GroG <supertick@gmail.com>
2016-02-16 06:24:12 -0800

    Update XMPP.py

--- service/XMPP.py
+++ service/XMPP.py
@@ -5,11 +5,12 @@
 xmpp = Runtime.createAndStart("xmpp","XMPP")
 
 # adds the python service as a listener for messages
-xmpp.addListener("python","publishMessage")
+# xmpp.addListener("python","publishMessage")
+python.subscribe("xmpp","publishMessage")
 
 # there is a big list of different xmpp/jabber servers out there
 # but we will connect to the big one - since that is where our robots account is
-xmpp.connect("talk.google.com", 5222, "robot01@myrobotlab.org", "xxxxxxx")
+xmpp.connect("myrobotlab.org", 5222, "GroG.robot01", "xxxxxxxx")
 
 # gets list of all the robots friends
 print xmpp.getRoster()
@@ -20,12 +21,11 @@ xmpp.setStatus(True, "online all the time")
 # add auditors you want this robot to chat with
 # auditors can issue commands and will be notified of 
 # commands being sent by others and what those commands return
-xmpp.addAuditor("Joe Smith")
-xmpp.addAuditor("Jane Smith")
+xmpp.addAuditor("GroG")
+# xmpp.addAuditor("Jane Smith")
 
 # send a message
-xmpp.sendMessage("hello this is robot01 - the current heatbed temperature is 40 degrees celcius", "Joe Smith")
+xmpp.sendMessage("hello this is robot01 - the current heatbed temperature is 40 degrees celcius", "GroG@myrobotlab.org")
 
-def publishMessage():
-	msg = msg_xmpp_publishMessage.data[0]
+def onMessage(msg):
 	print msg.getFrom(), " says " , msg.getBody()
