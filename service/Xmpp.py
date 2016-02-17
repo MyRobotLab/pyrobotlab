02e42b2f7ecbef0ad44d27bd1b10d91a117f2dcd
PapaoutaiRobot <giova1920@hotmail.it>
2016-02-17 15:48:20 +0100

    Small correction to use publish text

--- service/Xmpp.py
+++ service/Xmpp.py
@@ -6,7 +6,7 @@ xmpp = Runtime.createAndStart("xmpp","Xmpp")
 
 # adds the python service as a listener for messages
 # xmpp.addListener("python","publishMessage")
-python.subscribe("xmpp","publishMessage")
+python.subscribe("xmpp","publishXMPPMsg")
 
 # there is a big list of different xmpp/jabber servers out there
 # but we will connect to the big one - since that is where our robots account is
@@ -21,11 +21,11 @@ xmpp.setStatus(True, "online all the time")
 # add auditors you want this robot to chat with
 # auditors can issue commands and will be notified of 
 # commands being sent by others and what those commands return
-xmpp.addAuditor("GroG")
+xmpp.addAuditor("GroG@myrobotlab.org")
 # xmpp.addAuditor("Jane Smith")
 
 # send a message
 xmpp.sendMessage("hello this is robot01 - the current heatbed temperature is 40 degrees celcius", "GroG@myrobotlab.org")
 
-def onMessage(msg):
-	print msg.getFrom(), " says " , msg.getBody()
+def onXMPPMsg(xmppmsg):
+	print xmppmsg.msg.getFrom(), " says " , xmppmsg.msg.getBody()
