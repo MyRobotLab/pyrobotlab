d48786eab8d0b39d107e8b08a626f6b974f4faa0
theMastablasta <mastablasta@gmx.com>
2016-03-27 21:12:43 +0200

    Update startshort.py

--- home/Mastablasta/Python/startshort.py
+++ home/Mastablasta/Python/startshort.py
@@ -1,11 +1,19 @@
+
 import random
+from org.myrobotlab.service import Arduino
+from org.myrobotlab.service import Servo
+from org.myrobotlab.service import Runtime
 from java.lang import String
+from time import sleep
 holygrail = Runtime.createAndStart("holygrail", "WebGui")
 wksr = Runtime.createAndStart("webkitspeechrecognition", "WebkitSpeechRecognition")
-alice2 = Runtime.createAndStart("alice2", "ProgramAB")
-alice2.startSession("ProgramAB", "User", "alice2")
+simple = Runtime.createAndStart("simple", "ProgramAB")
+simple.startSession("simple")
 htmlfilter = Runtime.createAndStart("htmlfilter", "HtmlFilter")
-mouth = Runtime.createAndStart("i01.mouth", "MarySpeech")
-wksr.addTextListener(alice2)
-alice2.addTextListener(htmlfilter)
-htmlfilter.addTextListener(mouth)
+acapelaSpeech = Runtime.createAndStart("speech", "AcapelaSpeech")
+voices = acapelaSpeech.getVoices()
+for voice in voices:
+    acapelaSpeech.setVoice("Ryan")
+wksr.addTextListener(simple)
+simple.addTextListener(htmlfilter)
+htmlfilter.addTextListener(acapelaSpeech)
