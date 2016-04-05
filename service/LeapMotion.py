74abf5ec7a7979c964346e7204ef21bd5fc9d222
GroG <supertick@gmail.com>
2016-04-04 19:55:10 -0700

    refactoring

--- service/LeapMotion.py
+++ service/LeapMotion.py
@@ -1,17 +1,26 @@
+#####################################################
+#
+# this simple script will print in the python tab the grab strength 
+# of your left hand
+#
+# LeapMotion finger types are defined
+# as the following:
+# Type.TYPE_THUMB 
+# Type.TYPE_INDEX 
+# Type.TYPE_MIDDLE
+# Type.TYPE_RING 
+# Type.TYPE_PINKY
+
 from com.leapmotion.leap.Finger import Type
 # from __future__ import division
 
 leap = Runtime.createAndStart("leap","LeapMotion")
 
+leap.addFrameListener(python)
 
-leap.addListener("publishLeapData", "python", "onLeapData"); 
-# publishLeapData
-# leap.addFrameListener(python)
-
-def onLeapData(leapdata):
-  # print "Hello world"
-  print leapdata.leftHand.posX
-  print leapdata.leftHand.posY
-  print leapdata.leftHand.posZ
+def onFrame(frame):
+ 
+ strength = ((frame.hands().leftmost().grabStrength())*100)
+ print strength
 
 leap.startTracking()
