0b28c62d4cb9c6508880e53d770dd431f22b72b6
Anthony <moz4r@free.fr>
2016-04-25 23:19:25 +0200

    Update MyaiBot-Pictures.py (#16)
    add joke

--- home/moz4r/MyaiBot-Pictures.py
+++ home/moz4r/MyaiBot-Pictures.py
@@ -1,11 +1,33 @@
-#PICTURE FIND AND DISPLAY BOT
 #LONK AT http://www.myai.cloud/
 #FOR SERVER NAME AND BOT STATUS
-#IT S A SMALL COMPUTER FOR NOW SORRY IF PROBLEMS
+#IT S A SMALL COMPUTER FOR NOW
+
+# SETUP
+# ##############################################################################
+#Your language ( FR/IT/ES/RU etc .... )
+#Jokes only works in FRENCH for now please send me CSV !
+lang="FR"
+BotURL="http://myai.cloud/bot1.php"
+JokeType="CARAMBAR" # JokeType=BLONDES / CUL / CARAMBAR
+#désolé c'est vraiment des blagues pouries niveau CM2... faites tourner en csv pour alimenter
+#pas d'aprentissage pour le moment ca va venir . peut etre
+
+# ##############################################################################
+
+#dependencies
 from java.lang import String
 import random
 import threading
 import itertools
+import random
+
+
+BotURL=BotURL+"?lang="+lang
+
+
+#french margaux voice laugh emotions
+laugh = ["#LAUGH01#", "#LAUGH02#", "#LAUGH03#", ""]
+troat = ["#THROAT01#", "#THROAT02#", "#THROAT03#", ""]
 
 http = Runtime.createAndStart("http","HttpClient")
 Runtime.createAndStart("chatBot", "ProgramAB")
@@ -15,11 +37,9 @@ Runtime.createAndStart("htmlFilter", "HtmlFilter")
 Runtime.createAndStart("mouth", "AcapelaSpeech")  
 Runtime.createAndStart("Image", "ImageDisplay") 
 
-mouth.setVoice("Ryan")
-mouth.setLanguage("EN")
-#mouth.setVoice("Antoine")
-#mouth.setLanguage("FR")
-
+mouth.setVoice("Margaux")
+mouth.setLanguage("FR")
+ear.addTextListener(chatBot)
 chatBot.startSession( "default", "rachel") 
 chatBot.addTextListener(htmlFilter) 
 htmlFilter.addListener("publishText", python.name, "talk") 
@@ -28,11 +48,13 @@ def talk(data):
 	mouth.speak(data)
   	print "chatbot dit :", data
 def FindImage(image):
-	mouth.speak("I show you "+image)
-  #mouth.speak("Voici "+image)
+	mouth.speak("JE te montre "+image)
 	#PLEASE USE REAL LANGUAGE PARAMETER :
 	#lang=XX ( FR/EN/RU/IT etc...)
 	#A FAKE LANGUAGE WORKS BUT DATABASE WILL BROKE
-	a = String(http.get("http://myai.cloud/bot1.php?pic="+image.replace(" ", "%20")+"&lang=US"))
-	#a = String(http.get("http://myai.cloud/bot1.php?pic="+image.replace(" ", "%20")+"&lang=FR"))
+	a = String(http.get(BotURL+"&type=pic&pic="+image.replace(" ", "%20")))
 	Image.display(a)
+	
+def Joke():
+	a = http.get(BotURL+"&type=joke").replace(" : ", random.choice(troat)).decode( "utf8" )
+	mouth.speak(a+random.choice(laugh))
