8abd0a687612504d0fe8f6c1c890e0b98b2e3fdb
GroG <supertick@gmail.com>
2016-05-02 07:44:57 -0700

    Update Joystick.py

--- service/Joystick.py
+++ service/Joystick.py
@@ -1,2 +1,8 @@
 # start the service
 joystick = Runtime.start("joystick","Joystick")
+print(joystick.getControllers())
+python.subscribe("joystick","publishJoystickInput")
+joystick.setController(5)
+
+def onJoystickInput(data):
+  print(data)
