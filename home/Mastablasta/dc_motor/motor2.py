ff242213c80f4808bad1de704c5aeab8e0bb8fe4
theMastablasta <mastablasta@gmx.com>
2016-05-06 16:40:27 +0200

    Update motor2.py

--- home/Mastablasta/dc_motor/motor2.py
+++ home/Mastablasta/dc_motor/motor2.py
@@ -4,10 +4,10 @@ from time import sleep
 arduino = Runtime.createAndStart("arduino", "Arduino")
 arduino.serial.refresh()
 sleep(2)
-arduino.connect("COM8") 
+arduino.connect("COM5") 
 
 m1 = Runtime.start("m1","Motor")
-m1.setType2Pwm(10,11)
+m1.setType2Pwm(5,6)
 m1.attach(arduino)
 
 keyboard = Runtime.createAndStart("keyboard", "Keyboard")
@@ -19,13 +19,13 @@ global keyboardInput
 def Links(cmd):
     keyboardInput = "Links"
     print "motor left"
-    m1.move(1.0)
-    sleep(1)
+    m1.move(0.5)
+    sleep(0.2)
     m1.move(0.0)
 
 def Rechts(cmd):
     keyboardInput = "Rechts"
     print "motor right"
-    m1.move(-1.0)
-    sleep(1)
+    m1.move(-0.5)
+    sleep(0.2)
     m1.move(0.0)
