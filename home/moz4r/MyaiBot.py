a99d13f394f1b4e1cddbc058f7bb63231a677e8d
Anthony <moz4r@free.fr>
2016-05-06 02:35:39 +0200

    Update MyaiBot.py
    MRL 1335 fix
    + beetlejuice wikifetcher ( not work 4 now )

--- home/moz4r/MyaiBot.py
+++ home/moz4r/MyaiBot.py
@@ -1,8 +1,13 @@
 # ##############################################################################
 # 							*** SETUP ***
 # ##############################################################################
+# -----------------------------------
+# MayAi bot Version 0.9 By Moz4r
+# 
+# Wikidatafetcher By Beetlejuice
+# -----------------------------------
 #Your language ( FR/EN )
-lang="EN"
+lang="FR"
 BotURL="http://myai.cloud/bot1.php"
 #jokeBOT:
 JokeType="RANDOM" # JokeType=RANDOM / BLONDES / CUL / CARAMBAR
@@ -15,18 +20,19 @@ units="metric" # or : imperial
 #
 #IF YOU WANT INMOOV MOUTH CONTROL ( inmoov ) set inmoov=1
 #IF YOU DIDNT HAVE MOTORS set inmoov=0 
-IsInmoov=0
+IsInmoov=1
 leftPort = "COM3"
 rightPort = "COM4"
 jawMin = 45
 jawMax = 60
 # Ligh if you have
-IhaveLights = 0
+IhaveLights = 1
 MASSE=27
 ROUGE=29
 VERT=28
 BLEU=30
 # ###
+# 
 # ##############################################################################
 #  						*** END SETUP ***
 # ##############################################################################
@@ -44,6 +50,10 @@ import itertools
 import random
 import time
 import textwrap
+import codecs
+import socket
+import os
+
 
 
 BotURL=BotURL+"?lang="+lang+"&FixPhpCache="+str(time.time())
@@ -58,27 +68,19 @@ Runtime.createAndStart("ear", "WebkitSpeechRecognition")
 Runtime.createAndStart("webGui", "WebGui")
 Runtime.createAndStart("htmlFilter", "HtmlFilter")
 Runtime.createAndStart("Image", "ImageDisplay") 
+wdf=Runtime.createAndStart("wdf", "WikiDataFetcher")
+sleep(2)
 
 
-if IhaveLights==1:
-	right = Runtime.start("i01.right", "Arduino")
-	right.setBoard("mega2560")
-	right.connect(rightPort)
-	right.pinMode(MASSE, Arduino.OUTPUT)
-	right.pinMode(ROUGE, Arduino.OUTPUT)
-	right.pinMode(VERT, Arduino.OUTPUT)
-	right.pinMode(BLEU, Arduino.OUTPUT)
-	right.digitalWrite(MASSE,1)
-	right.digitalWrite(ROUGE,1)
-	right.digitalWrite(VERT,1)
-	right.digitalWrite(BLEU,1)
 
 
 if IsInmoov==0:
 	Runtime.createAndStart("mouth", "AcapelaSpeech")
 else:
 	i01 = Runtime.createAndStart("i01", "InMoov")
+	i01.startLeftHand(leftPort,"atmega2560")
 	i01.startMouth()
+	sleep(2)
 	mouth = i01.mouth
 	i01.startMouthControl(leftPort)
 	i01.head.jaw.setMinMax(jawMin,jawMax)
@@ -94,15 +96,38 @@ else:
 	i01.head.eyeY.rest()
 	i01.head.eyeX.rest()
 
+if IhaveLights==1:
+	i01.startLeftHand(rightPort,"atmega2560")
+	right = Runtime.start("i01.right", "Arduino")
+	right.setBoard("mega2560")
+	right.connect(rightPort)
+	sleep(2)
+	right.pinMode(MASSE, Arduino.OUTPUT)
+	right.pinMode(ROUGE, Arduino.OUTPUT)
+	right.pinMode(VERT, Arduino.OUTPUT)
+	right.pinMode(BLEU, Arduino.OUTPUT)
+	
+	right.publishState()
+	right.digitalWrite(MASSE,1)
+	right.digitalWrite(ROUGE,1)
+	right.digitalWrite(VERT,0)
+	right.digitalWrite(BLEU,1)	
+
 if lang=="FR":
    NoNo="Je ne comprend pas"
    voiceType="Margaux"
    ear.setLanguage("fr-FR")
+   wdf.setLanguage("fr")
+   wdf.setWebSite("frwiki")
 else:
    voiceType="Ryan"
    NoNo="I don't understand"
+   wdf.setLanguage("en")
+   wdf.setWebSite("enwiki")
 
-
+# on cherche en français
+ # On fait des recherches sur le site français de wikidata
+sleep(2)
 mouth.setVoice(voiceType)
 mouth.setLanguage(lang)
 ear.addTextListener(chatBot)
@@ -141,6 +166,59 @@ def Light(ROUGE_V,VERT_V,BLEU_V):
 def talk(data):
 	mouth.speak(data)
   	print "chatbot dit :", data
+	
+def askWiki(query): # retourne la description du sujet (query)
+	Light(1,0,0)
+	query = unicode(query,'utf-8')# on force le format de police UTF-8 pour prendre en charge les accents
+	if query[1]== "\'" : # Si le sujet contient un apostrophe , on efface tout ce qui est avant ! ( "l'été" -> "été")
+		query2 = query[2:len(query)]
+		query = query2
+	print query # petit affichage de contrôle dans la console python ..
+	word = wdf.cutStart(query) # on enlève le derminant ("le chat" -> "chat")
+	start = wdf.grabStart(query) # on garde que le déterminant ( je ne sais plus pourquoi j'ai eu besoin de ça, mais la fonction existe ...)
+	wikiAnswer = wdf.getDescription(word) # récupère la description su wikidata
+	answer = ( query + " est " + wikiAnswer)
+	if wikiAnswer == "Not Found !": # Si le document n'ai pas trouvé , on réponds "je ne sais pas"
+		answer = "Je ne sais pas"
+	chatBot.getResponse("say " + answer)	
+	Light(1,1,1)
+	
+
+
+def getProperty(query, what): # retourne la valeur contenue dans la propriété demandée (what)
+	Light(1,0,0)
+	query = unicode(query,'utf-8')
+	what = unicode(what,'utf-8')
+	if query[1]== "\'" :
+		query2 = query[2:len(query)]
+		query = query2
+	if what[1]== "\'" :
+		what2 = what[2:len(what)]
+		what = what2
+		print "what = " + what + " - what2 = " + what2
+	ID = "error"
+	# le fichier propriété.txt contient les conversions propriété -> ID . wikidata n'utilise pas des mots mais des codes (monnaie -> P38)	f = codecs.open(unicode('os.getcwd().replace("develop", "").replace("\", "/") + "/propriétés_ID.txt','r',"utf-8") #
+	f = codecs.open(u'c:/mrl/proprietes_ID.txt','r','utf-8') #os.getcwd().replace("develop", "").replace("\\", "/") set you propertiesID.txt path
+	
+	for line in f:
+    		line_textes=line.split(":")
+    		if line_textes[0]== what:
+	    		ID= line_textes[1]
+	f.close()
+	print "query = " + query + " - what = " + what + " - ID = " + ID
+	wikiAnswer= wdf.getData(query,ID) # récupère la valeur de la propriété si elle existe dans le document
+	answer = ( what +" de " + query + " est " + wikiAnswer)
+	
+	if wikiAnswer == "Not Found !":
+		answer = "Je ne sais pas"
+	chatBot.getResponse("say " + answer)
+	return answer
+	Light(1,1,1)
+	
+def getDate(query, ID):# Cette fonction permet d'afficher une date personnalisée (mardi, le 10 juin, 1975, 12h38 .....)
+	answer = ( wdf.getTime(query,ID,"day") +" " +wdf.getTime(query,ID,"month") + " " + wdf.getTime(query,ID,"year"))
+	print " La date est : " + answer
+	chatBot.getResponse("say Le " + answer)
 	
 def FindImage(image):
 	try:
@@ -226,7 +304,9 @@ def Meteo():
 
 def question(data):
 	a = Parse(BotURL+"&type=question&question="+urllib2.quote(data).replace(" ", "%20"))
-	mouth.speakBlocking(a[0:299])
-	mouth.speakBlocking(a[300:599])
+	if a[0:299]<>"":
+		mouth.speakBlocking(a[0:299])
+		mouth.speakBlocking(a[300:599])
 	MoveHead()
 i01.detach()
+Light(1,1,1)
