d22c43d7d2da023d09a765b467b537543f7c71e6
GroG <supertick@gmail.com>
2016-05-09 07:03:07 -0700

    Create BoofCV.py

---
+++ service/BoofCV.py
@@ -0,0 +1,8 @@
+#########################################
+# BoofCV.py
+# description: used as a general template
+# categories: [general]
+# possibly more info @: http://myrobotlab.org/service/BoofCV
+#########################################
+# start the service
+boofcv = Runtime.start("boofcv","BoofCV")
