0227986ca5f1f1f55a8c53456a9dfd6caf148430
GroG <supertick@gmail.com>
2016-05-09 06:45:42 -0700

    Create AdafruitIna219.py

---
+++ service/AdafruitIna219.py
@@ -0,0 +1,7 @@
+#########################################
+# AdafruitIna219.py
+# description: Adafruit INA219 Voltage and Current sensor Service
+# categories: [sensor]
+#########################################
+# start the service
+adafruitina219 = Runtime.start("adafruitina219","AdafruitIna219")
