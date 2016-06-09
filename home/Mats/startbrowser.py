cd53d9b9899464f21639dcd33eb6559cc31e4fe2
Mats <mats.onnerby@gmail.com>
2016-06-09 09:44:48 +0200

    Expanded the example with more services

--- home/Mats/startbrowser.py
+++ home/Mats/startbrowser.py
@@ -1,4 +1,15 @@
+# This is an example of staring two servo services
+# and showing each servo in a separate browser
+
+# Start the servo services
+s1 = Runtime.createAndStart("Servo1","Servo")
+s2 = Runtime.createAndStart("Servo2","Servo")
+
+# Start the webgui service without starting the browser
 webgui = Runtime.create("WebGui","WebGui")
 webgui.autoStartBrowser(False)
 webgui.startService()
-webgui.startBrowser("http://localhost:8888/#service/i01.ear")
+
+# Start two browsers and show one service in each
+webgui.startBrowser("http://localhost:8888/#service/Servo1")
+webgui.startBrowser("http://localhost:8888/#service/Servo2")
