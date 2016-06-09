1adb4207f84b50387d7c011f67fd0fa9959bb84f
Mats <mats.onnerby@gmail.com>
2016-06-09 10:20:35 +0200

    Update startbrowser.py
    Added sleep(1) to allow the broswe to start before

--- home/Mats/startbrowser.py
+++ home/Mats/startbrowser.py
@@ -1,5 +1,5 @@
 # This is an example of staring two servo services
-# and showing each servo in a separate browser
+# and showing each servo in a separate tab in the browsee
 
 # Start the servo services
 s1 = Runtime.createAndStart("Servo1","Servo")
@@ -10,6 +10,11 @@ webgui = Runtime.create("WebGui","WebGui")
 webgui.autoStartBrowser(False)
 webgui.startService()
 
-# Start two browsers and show one service in each
+# Start the browsers and show the first service ( Servo1 )
 webgui.startBrowser("http://localhost:8888/#service/Servo1")
+
+# Wait a little before executing the second startBrowser to allow
+# the first browser to start so that the second service will be shown
+# in a new tab. Without the sleep(1) you will probably get two browsers.
+sleep(1)
 webgui.startBrowser("http://localhost:8888/#service/Servo2")
