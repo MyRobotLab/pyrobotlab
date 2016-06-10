c5c7874a188eac5323ac185dedc572e46d06aaac
Mats <mats.onnerby@gmail.com>
2016-06-10 20:30:47 +0200

    Use AcapelaSpeech and WebkitSpeechRecognition

--- home/hairygael/InMoov2.minimal.py
+++ home/hairygael/InMoov2.minimal.py
@@ -4,11 +4,20 @@
 # a very minimal script for InMoov
 # although this script is very short you can still
 # do voice control of a right hand or finger box
-# for any command which you say - you will be required to say a confirmation
-# e.g. you say -> open hand, InMoov will ask -> "Did you say open hand?", you will need to
-# respond with a confirmation ("yes","correct","yeah","ya")
+# It uses WebkitSpeechRecognition, so you need to use Chrome as your default browser for this script to work
 
-rightPort = "COM13"
+# Start the webgui service without starting the browser
+webgui = Runtime.create("WebGui","WebGui")
+webgui.autoStartBrowser(False)
+webgui.startService()
+# Then start the browsers and show the WebkitSpeechRecognition service named i01.ear
+webgui.startBrowser("http://localhost:8888/#/service/i01.ear")
+
+# As an alternative you can use the line below to show all services in the browser. In that case you should comment out all lines above that starts with webgui. 
+# webgui = Runtime.createAndStart("webgui","WebGui")
+
+# Change to the port that you use
+rightPort = "COM4"
 
 i01 = Runtime.createAndStart("i01", "InMoov")
 i01.startEar()
@@ -16,7 +25,7 @@ i01.startEar()
 # starting parts
 i01.startMouth()
 #to tweak the default voice
-i01.mouth.setGoogleURI("http://thehackettfamily.org/Voice_api/api2.php?voice=Ryan&txt=")
+i01.mouth.setVoice("Ryan")
 ##############
 i01.startRightHand(rightPort)
 # tweaking defaults settings of right hand
@@ -43,11 +52,14 @@ ear.addCommand("close hand", "python", "handclose")
 ear.addCommand("capture gesture", ear.getName(), "captureGesture")
 ear.addCommand("manual", ear.getName(), "lockOutAllGrammarExcept", "voice control")
 ear.addCommand("voice control", ear.getName(), "clearLock")
- 
+
+# Confirmations and Negations are not supported yet in WebkitSpeechRecognition
+# So commands will execute immediatley
 ear.addComfirmations("yes","correct","yeah","ya")
 ear.addNegations("no","wrong","nope","nah")
 
 ear.startListening()
+i01.righthand.attach()
 
 def handopen():
   i01.moveHand("left",0,0,0,0,0)
