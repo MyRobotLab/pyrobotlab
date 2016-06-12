fa72b902b173f37a6ec56741a4b383beca218bc1
Mats <mats.onnerby@gmail.com>
2016-06-12 23:55:55 +0200

    Changed to use latest methods to connect

--- home/Mats/AdafruitServoDriver.py
+++ home/Mats/AdafruitServoDriver.py
@@ -1,18 +1,17 @@
-adaFruitRight = Runtime.createAndStart("adaFruitRight","Adafruit16CServoDriver")
-sleep(1)
-adaFruitRight.connect("COM4")
-# Set I2C address ( x40 = 4*16 + 0 = 64 )
-#adaFruitRight.setI2CAddress(64)
-sleep(1)
+webgui = Runtime.createAndStart("WebGui","WebGui")
+# Create and start the Arduino servicen and connect it to the serial port
+arduino = Runtime.createAndStart("Arduino","Arduino")
+arduino.connect("COM4")
+# Start the Adafruit16CServodriver that can be used for all PCA9685 devices
+# and connect it to the Arduino i2c interface using the default bus and
+# address
+adaFruit16c = Runtime.createAndStart("AdaFruit16C","Adafruit16CServoDriver")
+adaFruit16c.setController("Arduino")
 # create a new servo
 thumb = Runtime.createAndStart("Thumb", "Servo")
-sleep(1)
 elbow = Runtime.createAndStart("Elbow", "Servo")
-sleep(1)
 # attach it to the pwm board - pin 3 & 8
-sleep(1)
-thumb.attach(adaFruitRight,3)
-elbow.attach(adaFruitRight,8)
-#adaFruitRight.attach(thumb,3)
-thumb.setMinMax(8,142)
-elbow.setMinMax(0,180)
+thumb.attach(adaFruit16c,3)
+elbow.attach(adaFruit16c,8)
+# When this script has been executed you should be able to 
+# move the servos using the GUI or using python
