2f11c07f49bba583700c390e82e5895ffdf3590e
Anthony <moz4r@free.fr>
2016-07-19 02:28:23 +0200

    Bot version 1.5

--- home/moz4r/INMOOV-AI_paupieres_eyeleads.py
+++ home/moz4r/INMOOV-AI_paupieres_eyeleads.py
@@ -1,7 +1,7 @@
 from org.myrobotlab.service import Servo
 
 PaupiereServo = Runtime.start("PaupiereServo","Servo")
-PaupiereServo.setMinMax(0, 25)
+PaupiereServo.setMinMax(PaupiereMIN , PaupiereMAX)
 PaupiereServo.attach(right, PaupiereServoPin)
 
 
@@ -9,15 +9,15 @@ clock = Runtime.start("clock","Clock")
 clock.setInterval(1000)
 # define a ticktock method
 def ticktock(timedata):
-	PaupiereServo.moveTo(0)
+	PaupiereServo.moveTo(PaupiereMIN)
 	sleep(0.12)
-	PaupiereServo.moveTo(25)
+	PaupiereServo.moveTo(PaupiereMAX)
 #on fait un double clignement ou pas
 	if random.randint(0,1)==1:
 		sleep(0.2)
-		PaupiereServo.moveTo(0)
+		PaupiereServo.moveTo(PaupiereMIN)
 		sleep(0.12)
-		PaupiereServo.moveTo(25)
+		PaupiereServo.moveTo(PaupiereMAX)
 #on redefini une valeur aleatoire pour le prochain clignement
 	clock.setInterval(random.randint(10000,30000))
 #create a message routes
