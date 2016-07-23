2e50b08ea81f1731856b797f83d03fd58b9947cb
markusorngren <markus.orngren@gmail.com>
2016-07-23 18:29:47 +0200

    Update RGBSeriel.py

--- home/Markus/RGBSeriel.py
+++ home/Markus/RGBSeriel.py
@@ -12,10 +12,10 @@ def onJoystickInput(data):
 
         
     if (data.id=="R" and data.value == 1.0):
-        serial.write("255,0,0")  
+        serial.write("255,0,0\n")  
 
     if (data.id=="G" and data.value == 1.0):
-        serial.write("0,255,0") 
+        serial.write("0,255,0\n") 
 
     if (data.id=="B" and data.value == 1.0):
-        serial.write("0,0,255") 
+        serial.write("0,0,255\n") 
