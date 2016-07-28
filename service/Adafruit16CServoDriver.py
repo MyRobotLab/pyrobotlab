628c566be447f9c4a597f45563c5ff9cfe7f358e
Mats <mats.onnerby@gmail.com>
2016-07-28 22:46:49 +0200

    Update Adafruit16CServoDriver.py
    Updated to work on latest release ( 1561 )

--- service/Adafruit16CServoDriver.py
+++ service/Adafruit16CServoDriver.py
@@ -1,29 +1,25 @@
-# The Adafruit16CServoDriver API is supported through Jython 
-
-servo1 = Runtime.createAndStart("servo1", "Servo")	
-pwm =  Runtime.createAndStart("pwm", "Adafruit16CServoDriver")
-		
-pwm.connect("COM12")
-
-# attach servo1 to pin 0 on the servo driver
-pwm.attach(servo1, 0)
-
-servo1.broadcastState()
-
-servo1.moveTo(0)
-sleep(1)
-servo1.moveTo(90)
-sleep(1)
-servo1.moveTo(180)
-sleep(1)
-servo1.moveTo(90)
-sleep(1)
-servo1.moveTo(0)
-sleep(1)
-servo1.moveTo(90)
-sleep(1)
-servo1.moveTo(180)
-sleep(1)
-servo1.moveTo(90)
-sleep(1)
-servo1.moveTo(0)
+# Start the Adafruit16CServodriver that can be used for all PCA9685 devices
+adaFruit16c = Runtime.createAndStart("AdaFruit16C","Adafruit16CServoDriver")
+#
+# This part of the script is for the Arduino
+# Comment it out or delete it if you use the GPIO pins of the Raspberry PI
+# Change COM4 to the port where your Arduino is connected
+arduino = Runtime.createAndStart("Arduino","Arduino")
+arduino.connect("COM4")
+adaFruit16c.setController("Arduino","1","0x40")
+#
+# This part of the script is if you use the GPOI pins of the Raspberry PI
+# Comment it out or delete it if you use an Arduino
+raspi = Runtime.createAndStart("RasPi","RasPi")
+adaFruit16c.setController("RasPi","1","0x40")
+#
+# This part is common for both devices and creates two servo instances
+# on port 3 and 8 on the Adafruit16CServoDriver
+# Change the names of the servos and the pin numbers to your usage
+thumb = Runtime.createAndStart("Thumb", "Servo")
+elbow = Runtime.createAndStart("Elbow", "Servo")
+# attach it to the pwm board - pin 3 & 8
+thumb.attach(adaFruit16c,3)
+elbow.attach(adaFruit16c,8)
+# When this script has been executed you should be able to 
+# move the servos using the GUI or using python
