d2d2fd7114a65c40b85f18ea647641bf2420c6da
Langevin Gael <hairygael@gmail.com>
2016-08-01 20:18:59 +0200

    MyRobotLab.bat

---
+++ home/hairygael/mrl.bat
@@ -0,0 +1,6 @@
+taskkill.exe /F /IM java.exe
+taskkill.exe /F /IM javaW.exe 
+cd "C:\Myrobotlab\myrobotlab.1.0.1412\develop\ProgramAB\bots\inmoovWebKit\aimlif"
+del "learnf.aiml.csv"
+cd "C:\Myrobotlab\myrobotlab.1.0.1412"
+java -jar myrobotlab.jar -service gui GUIService python Python -invoke python execFile C:\Users\InMoovDeep\Documents\Arduino/InMoov3.Deep.AB.V4.py
