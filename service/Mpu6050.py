bfff31452e4c3f3972382aeaa157d6c7f82530b4
Mats <mats.onnerby@gmail.com>
2016-08-03 22:12:08 +0200

    Update Mpu6050.py

--- service/Mpu6050.py
+++ service/Mpu6050.py
@@ -4,6 +4,6 @@ raspi = Runtime.createAndStart("RasPi","RasPi")
 mpu6050 = Runtime.createAndStart("Mpu6050","Mpu6050")
 mpu6050.setController(raspi,"1","0x68")
 mpu6050.refresh()
-print mpu6050.filtered_x_angle;
-print mpu6050.filtered_y_angle;
-print mpu6050.filtered_z_angle;
+print mpu6050.filtered_x_angle
+print mpu6050.filtered_y_angle
+print mpu6050.filtered_z_angle
