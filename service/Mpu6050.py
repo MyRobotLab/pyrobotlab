3b67827ff5a81559e521a2ab9f837baccf67386c
Mats <mats.onnerby@gmail.com>
2016-08-03 22:11:28 +0200

    Update Mpu6050.py
    MPU6050 example code

--- service/Mpu6050.py
+++ service/Mpu6050.py
@@ -1,2 +1,9 @@
-# start the service
-mpu6050 = Runtime.start("mpu6050","Mpu6050")
+webgui = Runtime.createAndStart("WebGui","WebGui")
+raspi = Runtime.createAndStart("RasPi","RasPi")
+#
+mpu6050 = Runtime.createAndStart("Mpu6050","Mpu6050")
+mpu6050.setController(raspi,"1","0x68")
+mpu6050.refresh()
+print mpu6050.filtered_x_angle;
+print mpu6050.filtered_y_angle;
+print mpu6050.filtered_z_angle;
