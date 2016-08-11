712561420d793120165e07abc30680b9b20b214c
pedrosenarego <pedrogilsenarego@hotmail.com>
2016-08-11 17:19:54 +0100

    Create cmd.py

---
+++ home/pedrosenarego/cmd.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import subprocess
+subprocess.call("cd /home/pedro/Dropbox/pastaPessoal/3Dprinter/inmoov/scripts/googlecalandar && gnome-terminal -x python event.py", shell=True)
+
+
+
+ 
