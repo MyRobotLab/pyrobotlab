7ec258ece26ffb0bd560bf8be4fdad86ae618b43
Kevin Watters <kwatters@kmwllc.com>
2016-08-11 13:31:54 -0400

    Update cmd.py
    making this a function

--- home/pedrosenarego/cmd.py
+++ home/pedrosenarego/cmd.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import subprocess
-subprocess.call("cd /home/pedro/Dropbox/pastaPessoal/3Dprinter/inmoov/scripts/googlecalandar && gnome-terminal -x python event.py", shell=True)
 
+def runEvent(subject):
+  subprocess.call("cd /home/pedro/Dropbox/pastaPessoal/3Dprinter/inmoov/scripts/googlecalandar && gnome-terminal -x python event.py " + str(subject), shell=True)
+
+
+runEvent("It's party time!")
 
 
  
