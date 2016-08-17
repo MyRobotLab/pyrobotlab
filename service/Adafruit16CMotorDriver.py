ae44663e978417402aa58cf47a52e13a90ef60ea
Mats <mats.onnerby@gmail.com>
2016-08-17 22:48:29 +0200

    Create Adafruit16CMotorDriver.py
    Use Adafruit16CServoDriver to drive a motor

---
+++ service/Adafruit16CMotorDriver.py
@@ -0,0 +1,15 @@
+#
+# An example of how to use the Adafruit16CServoDriver to 
+# drive a motor.
+#
+# Runtime.start("webgui", "WebGui");
+arduino = Runtime.createAndStart("Arduino", "Arduino");
+arduino.connect("COM3")
+sleep(5)
+ada = Runtime.createAndStart("Ada","Adafruit16CServoDriver")
+ada.setController(arduino,"1","0x40")
+sleep(2)
+motor01 = Runtime.createAndStart("motor01", "Motor");
+motor01.setPwmPins(0,1);
+motor01.attach(ada);
+motor01.move(0.3);
