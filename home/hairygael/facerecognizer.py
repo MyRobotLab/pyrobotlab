bea5d2bfb3486ddac9827a24e4642f38544609ea
Langevin Gael <hairygael@gmail.com>
2016-09-03 23:13:13 +0200

    Update facerecognizer.py

--- home/hairygael/facerecognizer.py
+++ home/hairygael/facerecognizer.py
@@ -1,6 +1,6 @@
 def facerecognizer():
-    opencv.capture()
-    opencv.setDisplayFilter("FaceRecognizer")
+    i01.opencv.capture()
+    i01.opencv.setDisplayFilter("FaceRecognizer")
     fr.train()# it takes some time to train and be able to recognize face
     #if((lastName+"-inmoovWebKit" not in inmoovWebKit.getSessionNames())):
         #mouth.speak("Hello "+lastName)
