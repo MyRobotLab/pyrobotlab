dc0f1aca937fb7bcd1217de465b3eda4a86fe110
Langevin Gael <hairygael@gmail.com>
2016-09-14 16:19:50 +0200

    refactored

--- home/hairygael/batterylevel.py
+++ home/hairygael/batterylevel.py
@@ -1,5 +1,3 @@
 def batterylevel():
-  power_now = subprocess.check_output(["WMIC","PATH","Win32_Battery","Get","EstimatedChargeRemaining"])
-  battery = power_now.split("\n")[1].strip()
-  # print battery
-  i01.mouth.speak(str(battery) + " percent")
+	print(level)
+	i01.mouth.speak(str(level)+"percent")
