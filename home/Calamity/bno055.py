47c7cccc674beee06c2d4d6f6f197cb860d33354
Calamity01 <Calamity01@users.noreply.github.com>
2016-10-12 22:37:10 -0400

    Update bno055.py

--- home/Calamity/bno055.py
+++ home/Calamity/bno055.py
@@ -4,8 +4,9 @@ arduino.connect("COM11")
 bno = Runtime.createAndStart("bno","Bno055")
 bno.setController(arduino)
 if bno.begin():
-  event = bno.getEvent()
-  print event.orientation.x
-  print event.orientation.y
-  print event.orientation.z
-
+  while (True):
+    event = bno.getEvent()
+    print event.orientation.x
+    print event.orientation.y
+    print event.orientation.z
+    sleep(1)
