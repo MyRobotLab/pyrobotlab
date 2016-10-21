32fecf27acf1a597e98eb1919e70942fab294d39
brotherbrown831 <brotherbrown831@yahoo.com>
2016-10-21 12:35:54 -0500

    Update inmoov.prog.min.py

--- home/brotherbrown831/inmoov/inmoov.prog.min.py
+++ home/brotherbrown831/inmoov/inmoov.prog.min.py
@@ -10,17 +10,16 @@ def heard(data):
 ######################################################################
 # Create ProgramAB chat bot
 ######################################################################
-lloyd = Runtime.createAndStart("lloyd", "ProgramAB")
-# this starts a session between username "kevin" and the chat bot named
-# "alice2"  (AIML for the bots are in the develop/ProgramAB/bots directory.
-lloyd.startSession("kevin", "alice2")
+inmoov = Runtime.createAndStart("inmoov", "ProgramAB")
+# this starts a session between username "nolan" and the chat bot named
+# "inmoovwebkit"  (AIML for the bots are in the develop/ProgramAB/bots directory.
+inmoov.startSession("nolan", "inmoovwebkit")
 
 ######################################################################
 # create the speech recognition service
 # Speech recognition is based on WebSpeechToolkit API
 ######################################################################
 # Start the new WebGuiREST API for MRL
-
 webgui = Runtime.create("webgui","WebGui")
 webgui.autoStartBrowser(False)
 webgui.startService()
@@ -33,20 +32,7 @@ webgui.startBrowser("http://localhost:8888/#/service/i01.ear")
 # This service works in Google Chrome only with the WebGui
 ######################################################################
 wksr = Runtime.createAndStart("webkitspeechrecognition", "WebkitSpeechRecognition")
-######################################################################
-# create the html filter to filter the output of program ab
-# this service will strip out any html markup and return only the text
-# from the output of ProgramAB
-######################################################################
 htmlfilter = Runtime.createAndStart("htmlfilter", "HtmlFilter")
- 
-######################################################################
-# create the speech to text service (named the same as the inmoov's)
-# This service will listen to the output from the htmlfilter and
-# call out to the Acapela group to get the an MP3 that represents the
-# text to be spoken.  That mp3 will be played back by an AudioFile 
-# service.
-######################################################################
 mouth = Runtime.createAndStart("i01.mouth", "AcapelaSpeech")
 
 # debugging in python route.
@@ -55,9 +41,6 @@ mouth = Runtime.createAndStart("i01.mouth", "AcapelaSpeech")
 ######################################################################
 # MRL Routing webkitspeechrecognition -> program ab -> htmlfilter -> mouth
 ######################################################################
-# add a link between the webkit speech to publish text to ProgramAB
-wksr.addTextListener(lloyd)
-# Add route from Program AB to publish text to the html filter
-lloyd.addTextListener(htmlfilter)
-# Add route to publish text from html filter to mouth
+wksr.addTextListener(inmoov)
+inmoov.addTextListener(htmlfilter)
 htmlfilter.addTextListener(mouth)
