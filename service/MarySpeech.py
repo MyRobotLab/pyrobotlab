b9eabb953e0ad1b7ee170772316477fa13117850
Mats <mats.onnerby@gmail.com>
2016-11-01 23:18:37 +0100

    Update MarySpeech.py

--- service/MarySpeech.py
+++ service/MarySpeech.py
@@ -3,3 +3,6 @@ mouth.speakBlocking("Hello world")
 mouth.speakBlocking("I speak English. More voices are available, but they need to be installed")
 mouth.speakBlocking("Echo echo echo")
 mouth.speakBlocking("What should I use")
+mouth.speakBlocking("Happy birthday Kyle")
+                  
+                   
