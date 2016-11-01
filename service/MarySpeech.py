afcda62f222a6b401dfe1c552e4a4bb89c0c5252
Mats <mats.onnerby@gmail.com>
2016-11-01 23:16:39 +0100

    Update MarySpeech.py
    Added something to say

--- service/MarySpeech.py
+++ service/MarySpeech.py
@@ -1,2 +1,5 @@
-# start the service
-maryspeech = Runtime.start("maryspeech","MarySpeech")
+mouth = Runtime.createAndStart("MarySpeech", "MarySpeech")
+mouth.speakBlocking("Hello world")
+mouth.speakBlocking("I speak English. More voices are available, but they need to be installed")
+mouth.speakBlocking("Echo echo echo")
+mouth.speakBlocking("What should I use")
