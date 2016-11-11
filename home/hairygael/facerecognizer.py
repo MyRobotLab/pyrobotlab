0d5102f4c6d85c69303e68f9feafff8fc7b5a513
Langevin Gael <hairygael@gmail.com>
2016-11-11 04:18:53 +0100

    Delete facerecognizer.py

--- home/hairygael/facerecognizer.py
+++
@@ -1,8 +0,0 @@
-def facerecognizer():
-    i01.opencv.capture()
-    i01.opencv.setDisplayFilter("FaceRecognizer")
-    fr.train()# it takes some time to train and be able to recognize face
-    #if((lastName+"-inmoovWebKit" not in inmoovWebKit.getSessionNames())):
-        #mouth.speak("Hello "+lastName)
-        #sleep(2)
-    #inmoovWebKit.getResponse(lastName,data)
