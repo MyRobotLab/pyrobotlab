536aa669972bc3236e979c52ede97b62fcdd8fa6
Langevin Gael <hairygael@gmail.com>
2016-12-08 23:48:25 +0100

    Create VieAleatoire.py

---
+++ home/hairygael/GESTURES/WORKINPROGRESS/VieAleatoire.py
@@ -0,0 +1,13 @@
+def VieAleatoire():
+	global MoveHeadRandom
+	if MoveHeadRandom==1:
+	    MoveHeadTimer.startClock()
+	elif MoveHeadRandom==0:
+	    MoveHeadTimer.stopClock()
+	if MoveBodyRandom==1:
+	    MoveBodyTimer.startClock()
+	elif MoveBodyRandom==0:
+	    MoveBodyTimer.stopClock()
+
+	
+	
