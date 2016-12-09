c1b666405b97ca7c098c586f0a5523e4e879223f
Langevin Gael <hairygael@gmail.com>
2016-12-09 15:13:10 +0100

    Update InMoov3.hand+arm+rockpaperscissors.py

--- home/hairygael/InMoov3.hand+arm+rockpaperscissors.py
+++ home/hairygael/InMoov3.hand+arm+rockpaperscissors.py
@@ -23,13 +23,18 @@ human = 0
 # Change to the port that you use
 rightPort = "COM7"
 
+#to tweak the default voice
+Voice="cmu-slt-hsmm" # Default female for MarySpeech 
+#Voice="cmu-bdl" #Male US voice.You need to add the necessary file.jar to myrobotlab.1.0.XXXX/library/jar
+#https://github.com/MyRobotLab/pyrobotlab/blob/ff6e2cef4d0642e47ee15e353ef934ac6701e713/home/hairygael/voice-cmu-bdl-5.2.jar
+voiceType = Voice
+mouth = Runtime.createAndStart("i01.mouth", "MarySpeech")
+mouth.setVoice(voiceType)
+##############
+# starting parts
 i01 = Runtime.createAndStart("i01", "InMoov")
 i01.startEar()
-
-# starting parts
 i01.startMouth()
-#to tweak the default voice
-i01.mouth.setVoice("Ryan")
 ##############
 i01.startRightHand(rightPort)
 # tweaking defaults settings of right hand
