468a4c181768f0dcfcaa40201c26015b7c94e39e
Anthony <moz4r@free.fr>
2016-12-15 00:24:05 +0100

    add random gesture test

---
+++ home/moz4r/Test/random.py
@@ -0,0 +1,45 @@
+﻿import random
+from time import sleep
+
+i01 = Runtime.createAndStart("i01", "InMoov")
+i01.startHead("COM3")
+
+sleep(1)
+
+
+def MoveHeadRandomize():
+	if IcanMoveHeadRandom==1:
+		i01.moveHead(random.randint(50,130),random.randint(50,130))
+		
+
+MoveHeadTimer = Runtime.start("MoveHeadTimer","Clock")
+MoveHeadTimer.setInterval(1001)
+
+
+def MoveHead(timedata):
+
+	MoveHeadRandomize()
+	MoveHeadTimer.setInterval(random.randint(600,1200))
+		
+def MoveHeadStopped():
+
+	if IcanMoveHeadRandom==1:
+		i01.moveHead(90,90)
+		HeadSide.moveTo(90)
+		
+
+def MoveHeadStart():
+	MoveHeadRandomize()
+	
+MoveHeadTimer.addListener("pulse", python.name, "MoveHead")
+MoveHeadTimer.addListener("clockStopped", python.name, "MoveHeadStopped")		
+MoveHeadTimer.addListener("clockStarted", python.name, "MoveHeadStart")
+
+MoveHeadTimer.startClock()
+
+#start to move head random 10 seconds
+IcanMoveHeadRandom=1
+sleep(10)
+IcanMoveHeadRandom=0
+
+
