16228f60763f05f85c9038156c9f2355671b0166
Anthony <moz4r@free.fr>
2017-01-13 01:53:51 +0100

    Update marty.py

--- home/moz4r/Marty/marty.py
+++ home/moz4r/Marty/marty.py
@@ -15,21 +15,21 @@ cuisseGauche = Runtime.createAndStart("cuisseGauche", "Servo")
 genouGauche = Runtime.createAndStart("genouGauche", "Servo")
 chevilleGauche = Runtime.createAndStart("chevilleGauche", "Servo")
 sleep(1)
-cuisseDroite.enableAutoAttach(1)
-genouDroite.enableAutoAttach(1)
-chevilleDroite.enableAutoAttach(1)
-cuisseGauche.enableAutoAttach(1)
-genouGauche.enableAutoAttach(1)
-chevilleGauche.enableAutoAttach(1)
 
 
+cuisseDroiteRest=90
+genouDroiteRest=90
+chevilleDroiteRest=80
+cuisseGaucheRest=97
+genouGaucheRest=95
+chevilleGaucheRest=90
 
-cuisseDroite.setRest(90)
-genouDroite.setRest(90)
-chevilleDroite.setRest(80)
-cuisseGauche.setRest(97)
-genouGauche.setRest(95)
-chevilleGauche.setRest(90)
+cuisseDroite.setRest(cuisseDroiteRest)
+genouDroite.setRest(genouDroiteRest)
+chevilleDroite.setRest(chevilleDroiteRest)
+cuisseGauche.setRest(cuisseGaucheRest)
+genouGauche.setRest(genouGaucheRest)
+chevilleGauche.setRest(chevilleGaucheRest)
 
 
 
@@ -46,12 +46,21 @@ sleep(1)
 chevilleGauche.attach(adaFruit16c,6)
 sleep(1)
 
-cuisseDroite.setVelocity(35)
-genouDroite.setVelocity(35)
-chevilleDroite.setVelocity(35)
-cuisseGauche.setVelocity(35)
-genouGauche.setVelocity(35)
-chevilleGauche.setVelocity(35)
+cuisseDroite.enableAutoAttach(1)
+genouDroite.enableAutoAttach(1)
+chevilleDroite.enableAutoAttach(1)
+cuisseGauche.enableAutoAttach(1)
+genouGauche.enableAutoAttach(1)
+chevilleGauche.enableAutoAttach(1)
+
+
+
+cuisseDroite.setVelocity(30)
+genouDroite.setVelocity(30)
+chevilleDroite.setVelocity(30)
+cuisseGauche.setVelocity(30)
+genouGauche.setVelocity(30)
+chevilleGauche.setVelocity(30)
 
 
 cuisseDroite.rest()
@@ -63,17 +72,23 @@ chevilleGauche.rest()
 
 sleep(3)
 
-chevilleDroite.moveTo(90)
-chevilleGauche.moveTo(90)
-sleep(2)
-cuisseGauche.moveTo(60)
-sleep(2)
-chevilleDroite.moveTo(110)
-chevilleGauche.moveTo(110)
-sleep(2)
-cuisseGauche.moveTo(90)
-sleep(2)
-chevilleDroite.moveTo(70)
-chevilleGauche.moveTo(70)
+
+chevilleDroite.moveTo(chevilleDroiteRest+30)
+chevilleGauche.moveTo(chevilleGaucheRest+40)
+sleep(3)
+cuisseGauche.moveTo(cuisseGaucheRest+40)
+sleep(3)
+chevilleDroite.moveTo(chevilleDroiteRest)
+chevilleGauche.moveTo(chevilleGaucheRest)
+sleep(3)
+cuisseGauche.rest()
+sleep(5)
 
 
+
+cuisseDroite.rest()
+genouDroite.rest()
+chevilleDroite.rest()
+cuisseGauche.rest()
+genouGauche.rest()
+chevilleGauche.rest()
