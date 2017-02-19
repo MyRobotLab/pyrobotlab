cc679012adeac5a46809eca64f8592c00d6d9d17
Anthony <moz4r@free.fr>
2017-02-19 22:43:09 +0100

    Update stress_test1.py

--- home/moz4r/Test/stress_test1.py
+++ home/moz4r/Test/stress_test1.py
@@ -6,23 +6,26 @@ import random
 leftPort = "COM3"
 
 i01 = Runtime.createAndStart("i01", "InMoov")
+
 sleep(1)
 i01.startMouth()
-i01.startHead(leftPort)
 i01.startLeftHand(leftPort)
-i01.head.jaw.map(0,180,85,110)
-i01.startMouthControl(leftPort)
+sleep(1)
 
 
-i01.leftHand.thumb.setVelocity(random.randint(100,300))
+i01.mouth.speakBlocking("voice test voice test")
+i01.mouth.speakBlocking("voice test voice test")
+
+i01.leftHand.thumb.setVelocity(random.uniform(100,300))
 	
 MoveRandomTimer = Runtime.start("MoveRandomTimer","Clock")
 
 def MoveRandom(timedata):
 	
-	i01.leftHand.thumb.moveTo(random.randint(50,130))
-	MoveRandomTimer.setInterval(random.randint(10000,11000))
+	i01.leftHand.thumb.moveTo(random.uniform(10.1,140.2))
+	MoveRandomTimer.setInterval(random.randint(1000,2000))
 	i01.mouth.speak("voice test voice test")
 	
 MoveRandomTimer.addListener("pulse", python.name, "MoveRandom")
 MoveRandomTimer.startClock()
+	
