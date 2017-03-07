b8c2ec090a76b05de3d26c43da862da77b5ebe43
Mats <mats.onnerby@gmail.com>
2017-03-07 17:10:18 +0100

    Update Max9744.py
    Fixed some spelling errors

--- home/Mats/Max9744.py
+++ home/Mats/Max9744.py
@@ -1,6 +1,6 @@
-# Script to change the voulme of the Max8744
+# Script to change the volume of the Max9744
 # It's similar to the pcf8574 in that it only writes a single byte
-# The voume is controlled by writing a value between 0 and 63
+# The volume is controlled by writing a value between 0 and 63
 volume = 16
 arduino = Runtime.start("arduino","Arduino")
 arduino.connect("COM8")
