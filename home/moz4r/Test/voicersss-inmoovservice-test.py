135432904e28bbed7edf511cc8284b33f7c6c748
Anthony <moz4r@free.fr>
2017-03-16 11:38:05 +0100

    Update voicersss-inmoovservice-test.py

--- home/moz4r/Test/voicersss-inmoovservice-test.py
+++ home/moz4r/Test/voicersss-inmoovservice-test.py
@@ -9,6 +9,6 @@ def onEndSpeaking(text):
 def onStartSpeaking(text):
 	print "start speak"
 
-i01.mouth.setKey("6b714718f09e48c9a7f260e385ca99a4")
+i01.mouth.setKey("!!! YOUR_KEY !!!")
 i01.mouth.setVoice("fr-fr");
 i01.mouth.speakBlocking(u"test accent utf8 : éléphant")
