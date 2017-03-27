6a68f2c04f395eba3114e49102001624aa8de783
kwatters <kwatters@kmwllc.com>
2017-03-27 15:08:40 -0400

    some updates to show examples of python syntax and the leap motion data.

--- home/pedrosenarego/leapMotionLeapData.py
+++ home/pedrosenarego/leapMotionLeapData.py
@@ -1,27 +1,43 @@
-from com.leapmotion.leap.Finger import Type
-# from __future__ import division
+##################################################################
+# Leap Motion Example Script
+##################################################################
 
-#####################################################
-#
-# this simple script will print in the python tab the grab strength 
-# of your left hand
-#
-# LeapMotion finger types are defined
-# as the following:
-# Type.TYPE_THUMB 
-# Type.TYPE_INDEX 
-# Type.TYPE_MIDDLE
-# Type.TYPE_RING 
-# Type.TYPE_PINKY
-
-leap = Runtime.createAndStart("leap","LeapMotion")
-
-leap.addLeapDataListener(python)
-
+# when leap motion data is detected, it will be passed in here
 def onLeapData(data):
- 
- 
- print (data.rightHand.index)
- 
-leap.startTracking()
+  if (data.rightHand):
+    # if the data has a right hand, print out some info about it.
+    print("Right Thumb =" + str(data.rightHand.thumb))   
+    print("Right Index =" + str(data.rightHand.index))
+    print("Right Middle =" + str(data.rightHand.middle))
+    print("Right Ring =" + str(data.rightHand.ring))
+    print("Right Pinky =" + str(data.rightHand.pinky))
+    print("Right Hand Position: x=" + str(data.rightHand.posX) + " y=" + str(data.rightHand.posY) + " z=" + str(data.rightHand.posZ))
+  else:
+    # the right hand wasn't found.
+    print("Right hand not detected.")
+    
+  if (data.leftHand):
+    # print some info about the left hand
+    print("Left Thumb =" + str(data.leftHand.thumb))   
+    print("Left Index =" + str(data.leftHand.index))
+    print("Left Middle =" + str(data.leftHand.middle))
+    print("Left Ring =" + str(data.leftHand.ring))
+    print("Left Pinky =" + str(data.leftHand.pinky))
+    print("Left Hand Position: x=" + str(data.leftHand.posX) + " y=" + str(data.leftHand.posY) + " z=" + str(data.leftHand.posZ))
+  else: 
+    # print left hand not found.
+    print("Left hand not detected.")
 
+  if (data.frame):
+    # this is the raw frame info from the leap if you want it.
+    print(str(frame))
+    
+###########################################################
+# MAIN Script entry point
+###########################################################
+# create the leap motion service
+leap = Runtime.createAndStart("leap","LeapMotion")
+# connect python as a listener for the onLeapData callback
+leap.addLeapDataListener(python)
+# start the leap motion watching for valid frames.
+leap.startTracking()
