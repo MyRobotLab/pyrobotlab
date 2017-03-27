65a7fe37b94ef1ed46df862a149fb23d2c2d8eec
kwatters <kwatters@kmwllc.com>
2017-03-27 15:18:58 -0400

    updating the example showing to move a servo when you move your right index finger.

--- home/pedrosenarego/leapMotionLeapData.py
+++ home/pedrosenarego/leapMotionLeapData.py
@@ -4,6 +4,10 @@
 
 # when leap motion data is detected, it will be passed in here
 def onLeapData(data):
+  # make sure we get the global scope of the servo service here.
+  global servo01
+  # process the data that came in.
+  # right hand first
   if (data.rightHand):
     # if the data has a right hand, print out some info about it.
     print("Right Thumb =" + str(data.rightHand.thumb))   
@@ -12,10 +16,12 @@ def onLeapData(data):
     print("Right Ring =" + str(data.rightHand.ring))
     print("Right Pinky =" + str(data.rightHand.pinky))
     print("Right Hand Position: x=" + str(data.rightHand.posX) + " y=" + str(data.rightHand.posY) + " z=" + str(data.rightHand.posZ))
+    # update a position of the servo ..
+    servo01.moveTo(data.rightHand.index)
   else:
     # the right hand wasn't found.
     print("Right hand not detected.")
-    
+  # left hand data.
   if (data.leftHand):
     # print some info about the left hand
     print("Left Thumb =" + str(data.leftHand.thumb))   
@@ -41,3 +47,25 @@ leap = Runtime.createAndStart("leap","LeapMotion")
 leap.addLeapDataListener(python)
 # start the leap motion watching for valid frames.
 leap.startTracking()
+
+# set the servo pin that we'll control
+servoPin = 3
+# specify a rest postion for the servo
+restPosition = 90
+# specify a com port for the arduino
+comPort = "/dev/ttyACM0"
+
+# create the servo & arduino services
+arduino = Runtime.start("arduino","Arduino")
+servo01 = Runtime.start("servo01","Servo")
+
+arduino.connect(comPort)
+# TODO - set limits
+servo01.setMinMax(30, 143)
+servo01.map(0,180,30,143)
+servo01.setVelocity(-1)
+# attach servo
+servo01.attach(arduino.getName(), servoPin)
+# lets move the servo to it's rest position.
+servo01.moveTo(restPosition)
+
