72355cde5cb82843584e00acab3f5edfeb912d3a
pedrosenarego <pedrogilsenarego@hotmail.com>
2017-05-05 22:21:06 +0100

    Add files via upload

---
+++ home/pedrosenarego/leapMotionLoopFromTxt.py
@@ -0,0 +1,29 @@
+import csv
+
+f = open("Index.csv", "rb")
+reader = csv.reader(f, delimiter='\t', lineterminator='\n',)
+columns = list(reader)
+   
+  
+# set the servo pin that we'll control
+servoPin = 3
+# specify a rest postion for the servo
+restPosition = 90
+# specify a com port for the arduino
+comPort = "/dev/ttyACM0"
+
+# create the servo & arduino services
+arduino = Runtime.start("arduino","Arduino")
+servo01 = Runtime.start("servo01","Servo")
+
+arduino.connect(comPort)
+# TODO - set limits
+servo01.setMinMax(0, 180)
+servo01.map(0, 180, 0, 180)
+servo01.setVelocity(-1)
+# attach servo
+servo01.attach(arduino.getName(), servoPin)
+# lets move the servo to it's rest position.
+
+servo01.moveTo(columns)
+sleep(0.5)
