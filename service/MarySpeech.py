7059b4726ba8ffa36396c386d84e5f1f7f1b6517
LunDev <LunDev@users.noreply.github.com>
2017-05-12 21:55:58 +0200

    add more information to MarySpeech example script

--- service/MarySpeech.py
+++ service/MarySpeech.py
@@ -1,8 +1,23 @@
+#start Service
 mouth = Runtime.createAndStart("MarySpeech", "MarySpeech")
+
+#speak!
 mouth.speakBlocking("Hello world")
 mouth.speakBlocking("I speak English. More voices are available, but they need to be installed")
 mouth.speakBlocking("Echo echo echo")
 mouth.speakBlocking("What should I use")
 mouth.speakBlocking("Happy birthday Kyle")
-                  
-                   
+
+#install a voice:
+#an overview over all official voices is available @ http://myrobotlab.org/service/MarySpeech
+#mouth.installComponentsAcceptLicense(voicename)
+#e.g.
+#mouth.installComponentsAcceptLicense("bits1")
+
+#switch voice:
+#mouth.setVoice(voicename)
+#mouth.setVoice("bits1")
+
+#add voice effects:
+#more effects and information @ http://myrobotlab.org/service/MarySpeech
+mouth.setAudioEffects("FIRFilter+Robot(amount=50)");
