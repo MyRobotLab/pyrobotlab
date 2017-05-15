3c6cfa0fdbbfe224dc732081d592bcc90f5fea73
Anthony <moz4r@free.fr>
2017-05-15 18:41:25 +0200

    Create OpenWeatherMap.py

---
+++ service/OpenWeatherMap.py
@@ -0,0 +1,12 @@
+OpenWeatherMap=Runtime.createAndStart("OpenWeatherMapTranslator", "OpenWeatherMap")
+OpenWeatherMap.setApiKey("YOUR_KEY") #https://home.openweathermap.org/
+OpenWeatherMap.setUnits("metric") # or imperial
+OpenWeatherMap.setLang("fr") # en / de ...
+
+# fetch raw data:
+r=[]
+r=OpenWeatherMap.fetchRaw("YOUR CITY")
+print r[0],r[1],r[2]
+
+# fetch complete sentence data:
+print OpenWeatherMap.fetchWeather("YOUR CITY")
