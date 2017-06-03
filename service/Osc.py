86d95707b6a27424a5acd9e240dae737d1b11d90
grperry <grperry@CTNAL0043108539.us164.corpintra.net>
2017-06-03 09:43:57 -0700

    fixed recv bug

--- service/Osc.py
+++ service/Osc.py
@@ -4,17 +4,17 @@ osc = Runtime.start("osc","Osc")
 # connect - which is not 'really' connecting - but
 # specifying the host/port of where we'll be sending 
 # the messages to
-osc.connect("localhost", 12000);
+osc.connect("localhost", 12000)
 
 # now start sending messages 
 # the format is
 # sendMsg(topic, arg1, arg2, arg3, ...)
-osc.sendMsg("/test", "this is a string", 3, 7.5, "another string");
-osc.sendMsg("/newTopic", 18, "hello", 4.5);
-osc.sendMsg("/somewhere/else", "this", "is", 7, 3.3, "variable arguments");
+osc.sendMsg("/test", "this is a string", 3, 7.5, "another string")
+osc.sendMsg("/newTopic", 18, "hello", 4.5)
+osc.sendMsg("/somewhere/else", "this", "is", 7, 3.3, "variable arguments")
 
 # to listen we choose a port
-osc.listen(6000)
+osc.listen("/test", 6000)
 
 # we want them sent to python so we subscribe to
 # the publishOSCMessage method
