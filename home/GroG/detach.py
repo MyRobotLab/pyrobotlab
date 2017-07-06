7b570bd66adfeb682818423564c7044ac4e244ab
Mats <mats.onnerby@gmail.com>
2017-07-06 17:25:19 +0200

    Update detach.py
    Added the rest of the script

--- home/GroG/detach.py
+++ home/GroG/detach.py
@@ -5,4 +5,6 @@ adafruit = Runtime.start('adafruit','Adafruit16CServoDriver')
 arduino = Runtime.start('arduino','Arduino')
 servo = Runtime.start('servo','Servo')
 arduino.connect(port)
-
+adafruit.setController(arduino,"0","0x40")
+servo.attach(adafruit,3) 
+# Now close the GUI
