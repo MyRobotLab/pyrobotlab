3ced0a8e3ffae3883302b4ed9addb00b613b6a17
GroG <supertick@gmail.com>
2017-07-06 08:13:01 -0700

    Update detach.py

--- home/GroG/detach.py
+++ home/GroG/detach.py
@@ -3,4 +3,6 @@ virtual = Runtime.start('virtual','VirtualArduino')
 virtual.connect(port)
 adafruit = Runtime.start('adafruit','Adafruit16CServoDriver')
 arduino = Runtime.start('arduino','Arduino')
+servo = Runtime.start('servo','Servo')
 arduino.connect(port)
+
