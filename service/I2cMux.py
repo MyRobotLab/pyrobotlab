bee8f8e090d16bb1077a95e4a864729c4f9ef7e8
Mats <mats.onnerby@gmail.com>
2017-07-13 02:05:56 +0200

    From version 1.0.2316 use attach instead of setController

--- service/I2cMux.py
+++ service/I2cMux.py
@@ -3,10 +3,12 @@ ard = Runtime.createAndStart("Arduino","Arduino")
 ard.connect("COM3")
 #
 i2cmux = Runtime.createAndStart("i2cMux","I2cMux")
+# From version 1.0.2316 use attach instead of setController
 i2cmux.setController(ard,"1","0x70")
+i2cmux.attach(ard,"1","0x70")
 #
 mpu6050_0 = Runtime.createAndStart("Mpu6050-0","Mpu6050")
-mpu6050_0.setController(i2cmux,"0","0x68")
+mpu6050_0.attach(i2cmux,"0","0x68")
 
 mpu6050_1 = Runtime.createAndStart("Mpu6050-1","Mpu6050")
-mpu6050_1.setController(i2cmux,"1","0x68")
+mpu6050_1.attach(i2cmux,"1","0x68")
