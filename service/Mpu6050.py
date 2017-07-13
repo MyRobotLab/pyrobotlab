cd65423516acce317286717cd3ae4eda0f23e1a5
Mats <mats.onnerby@gmail.com>
2017-07-13 02:06:35 +0200

    From version 1.0.2316 use attach instead of setController

--- service/Mpu6050.py
+++ service/Mpu6050.py
@@ -2,7 +2,9 @@ webgui = Runtime.createAndStart("WebGui","WebGui")
 raspi = Runtime.createAndStart("RasPi","RasPi")
 #
 mpu6050 = Runtime.createAndStart("Mpu6050","Mpu6050")
-mpu6050.setController(raspi,"1","0x68")
+# From version 1.0.2316 use attach instead of setController
+# mpu6050.setController(raspi,"1","0x68")
+mpu6050.attach(raspi,"1","0x68")
 mpu6050.refresh()
 print mpu6050.filtered_x_angle
 print mpu6050.filtered_y_angle
