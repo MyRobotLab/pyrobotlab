7ef491d8dbbb60ce1b8461be421bc7b493bbd94b
Anthony <moz4r@free.fr>
2017-08-04 12:43:40 +0200

    Update MicrosoftSpeech.py

--- service/MicrosoftSpeech.py
+++ service/MicrosoftSpeech.py
@@ -1,10 +1,10 @@
 #this is WIP
 tts = Runtime.start("tts", "MicrosoftLocalTTS");
 
-#get available system voice for information ( check id )
+#get available system voices for information ( check id )
 print tts.getVoices();
 
 tts.setVoice("0")
 tts.speakBlocking(u"Bonjour ceci est une voix française")
 tts.setVoice("1")
-tts.speakBlocking(u"Hello this is an english voice")
+tts.speak(u"Hello this is an english voice")
