6199990c386e2b7b53d1cf4a83a98d22fa9ca3e2
Perry <grperry@americas.corpdir.net>
2017-08-27 12:28:09 -0700

    added codeSample.py to master

---
+++ home/GroG/codeSample.py
@@ -0,0 +1,42 @@
+# MASTER RELEASED STABLE SCRIPT SCRIPT !!!!!
+# this is my most awesome code sample on the develop branch
+# its cutting edge - but if you want to see it 
+# you supply ?branch=develop at the end of the documentation url
+
+useVirtualArduino = False;
+xPin = 9;
+yPin = 6;
+arduinoPort = "COM5";
+cameraIndex = 0;
+# using Sarxos for usb webcam, the other frame grabbers only worked on my integrated camera
+frameGrabberType = "org.myrobotlab.opencv.SarxosFrameGrabber";
+
+Runtime.start("gui", "SwingGui");
+
+if useVirtualArduino:
+  virtual = Runtime.start("virtual", "VirtualArduino");
+  virtual.connect(arduinoPort);
+
+
+t01 = Runtime.start("t01", "Tracking");
+x = t01.getX();
+# invert if necessary
+# x.setInverted(True);
+
+y = t01.getY();
+# invert if necessary
+# y.setInverted(True);
+
+t01.connect(arduinoPort, xPin, yPin, cameraIndex);
+opencv = t01.getOpenCV();
+# noticed some swing display issues - I don't think Sarxos gets updated to display
+opencv.setFrameGrabberType(frameGrabberType);
+opencv.broadcastState();
+
+# not sure if necessary - but get things to settle for 3 seconds
+# before starting tracking
+sleep(3);
+# do lk optical point tracking
+# t01.startLKTracking();
+# do face tracking
+t01.faceDetect();
