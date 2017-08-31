3e99a214295cdafb7ade88ed3e56fd8f05dc119f
grog <grog@myrobotlab.org>
2017-08-31 08:02:16 -0700

    missing service scripts

---
+++ service/WebGuiClient.py
@@ -0,0 +1,9 @@
+#########################################
+# WebGuiClient.py
+# description: used as a general webguiclient
+# categories: general
+# more info @: http://myrobotlab.org/service/WebGuiClient
+#########################################
+
+# start the service
+webguiclient = Runtime.start('webguiclient','WebGuiClient')
