bdcbf11a424140d2fc3ceca22ae663e9c9664791
Anthony <moz4r@free.fr>
2017-09-16 01:48:47 +0200

    Update Clock.py

--- service/Clock.py
+++ service/Clock.py
@@ -7,13 +7,14 @@ audio = Runtime.start("audio","AudioFile")
 # define a ticktock method
 def ticktock(timedata):
     print timedata
-    audio.playResource("/resource/Clock/tick.mp3")
+    audio.playResource("resource/Clock/tick.mp3")
 
 #create a message routes
 clock.addListener("pulse", python.name, "ticktock")
 clock.addListener("pulse","log","log")
 
 # start the clock
+clock.setInterval(1000)
 clock.startClock()
 # optional : wait the first loop before execution start
 # clock.startClock(1)
