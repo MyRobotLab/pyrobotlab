f71e04ca82c7076e5b1e116c8b3d888305d3e285
moz4r ( Anthony ) <moz4r@free.fr>
2017-09-20 18:52:55 +0200

    mop tracking.py

--- service/Tracking.py
+++ service/Tracking.py
@@ -9,16 +9,16 @@ port = "COM19"   #change COM port to your own port
 xServoPin = 13   #change this to the right servo pin if needed, for inmoov this is right
 yServoPin = 12   #change this to the right servo pin if needed, for inmoov this is right
 
-tracker = Runtime.createAndStart("tracker", "Tracking")
+tracker = Runtime.start("tracker", "Tracking")
+gui = Runtime.start("gui", "SwingGui")
+opencv=tracker.getOpenCV()
 
 # set specifics on each Servo
 servoX = tracker.getX()
-servoX.setPin(xServoPin)
 servoX.setMinMax(30, 150)  #minimum and maximum settings for the X servo
 # servoX.setInverted(True) # invert if necessary
 
 servoY = tracker.getY()
-servoY.setPin(yServoPin)
 servoY.setMinMax(30, 150)  #minimum and maximum settings for the Y servo
 # servoY.setInverted(True) # invert if necessary
 
@@ -35,11 +35,25 @@ servoY.setMinMax(30, 150)  #minimum and maximum settings for the Y servo
 # optional filter settings
 opencv = tracker.getOpenCV()
 
-# setting camera index to 1 default is 0
-opencv.setCameraIndex(1) 
+# not for you, it's for test
+if ('virtual' in globals() and virtual):
+  virtualArduino = Runtime.start("virtualArduino", "VirtualArduino")
+  virtualArduino.connect(port)
+  
+# connect to the Arduino ( 0 = camera index )
+tracker.connect(port, xServoPin, yServoPin, 0)
 
-# connect to the Arduino
-tracker.connect(port)
+if ('virtual' in globals() and virtual):
+  opencv.stopCapture()
+  opencv.setMinDelay(500)
+  opencv.setFrameGrabberType("org.bytedeco.javacv.FFmpegFrameGrabber")
+  opencv.setInputSource("file")
+  opencv.setInputFileName("resource/OpenCV/testData/monkeyFace.mp4")
+
+# gui unfreeze
+gui.undockTab("tracker.opencv")
+opencv.broadcastState();
+sleep(1)
 
 # Gray & PyramidDown make face tracking
 # faster - if you dont like these filters - you
@@ -49,12 +63,11 @@ tracker.connect(port)
 
 # diffrent types of tracking
 
-# simple face detection and tracking
-# tracker.faceDetect()
-
 # lkpoint - click in video stream with 
 # mouse and it should track
-tracker.startLKTracking()
+# simple point detection and tracking
+# tracker.startLKTracking()
+tracker.faceDetect()
 
 # scans for faces - tracks if found
-# tracker.findFace() 
+# tracker.findFace()
