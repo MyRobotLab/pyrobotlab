01b474c0a6fd718152664fc1fe9d9cc24772d63d
Anthony <moz4r@free.fr>
2017-09-21 23:32:51 +0200

    mop WebkitSpeechRecognition.py

--- service/WebkitSpeechRecognition.py
+++ service/WebkitSpeechRecognition.py
@@ -1,25 +1,32 @@
 #########################################
 # WebkitSpeechRecognition.py
 # more info @: http://myrobotlab.org/service/WebkitSpeechRecognition
+# you need chrome browser as default
 #########################################
 
-# start the service
+# Start the webgui service without starting the browser
+webgui = Runtime.create("WebGui","WebGui")
+webgui.autoStartBrowser(False)
+webgui.startService()
+
+# Then start the browsers and show the WebkitSpeechRecognition service named webkitspeechrecognition
+webgui.startBrowser("http://localhost:8888/#/service/webkitspeechrecognition")
 webkitspeechrecognition = Runtime.start("webkitspeechrecognition","WebkitSpeechRecognition")
+webkitspeechrecognition.setLanguage("en")
+# start mouth
+marySpeech = Runtime.start("marySpeech", "MarySpeech")
+
 # auto rearm microphone
-# webkitspeechrecognition.setAutoListen(True)
-# speedup recognition
-# webkitspeechrecognition.setContinuous(False)
-=======
-webgui = Runtime.start("webgui","WebGui")
-webkitspeechrecognition = Runtime.start("webkitspeechrecognition","WebkitSpeechRecognition")
-arduino = Runtime.start("arduino","Arduino")
-arduino.connect("COM3")
+webkitspeechrecognition.setAutoListen(False)
+
+# speedup recognition if False
+webkitspeechrecognition.setContinuous(False)
 
 def lightOn():
-  arduino.digitalWrite(13,1)
+  marySpeech.speakBlocking("light is on")
 
 def lightOff():
-  arduino.digitalWrite(13,0)
+  marySpeech.speakBlocking("light is off")
 
 def onText(data):
      print data
