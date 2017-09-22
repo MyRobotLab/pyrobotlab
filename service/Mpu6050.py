76e66022566e3f9d2faee40c2c4db6167af38292
moz4r ( Anthony ) <moz4r@free.fr>
2017-09-22 17:44:58 +0200

    mop mpu6050.py

--- service/Mpu6050.py
+++ service/Mpu6050.py
@@ -1,10 +1,24 @@
-WebGui = Runtime.createAndStart("WebGui","WebGui")
-raspi = Runtime.createAndStart("RasPi","RasPi")
-#
+#########################################
+# Mpu6050.py
+# more info @: http://myrobotlab.org/service/Mpu6050
+#########################################
+# port = "/dev/ttyUSB0"
+port = "COM3"
+
 mpu6050 = Runtime.createAndStart("Mpu6050","Mpu6050")
-# From version 1.0.2316 use attach instead of setController
-# mpu6050.setController(raspi,"1","0x68")
-mpu6050.attach(raspi,"1","0x68")
+# start optional virtual arduino service, used for test
+# virtual = True
+if ('virtual' in globals() and virtual):
+    virtualArduino = Runtime.start("virtualArduino", "VirtualArduino")
+    virtualArduino.connect(port)
+# end test
+# raspi controler :
+# raspi = Runtime.createAndStart("RasPi","RasPi")
+arduino = Runtime.start("arduino","Arduino")
+arduino.connect(port)
+
+# mpu6050.attach(raspi,"1","0x68")
+mpu6050.attach(arduino,"1","0x68")
 mpu6050.refresh()
 print mpu6050.filtered_x_angle
 print mpu6050.filtered_y_angle
