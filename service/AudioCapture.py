72ad6a987923c75ea97119dc4238d7d832c596a3
GroG <supertick@gmail.com>
2017-09-27 06:55:21 -0700

    Update AudioCapture.py

--- service/AudioCapture.py
+++ service/AudioCapture.py
@@ -1,7 +1,30 @@
-from org.myrobotlab.service import Runtime
-from org.myrobotlab.service import AudioCapture
-from time import sleep
 audiocapture = Runtime.createAndStart("audiocapture","AudioCapture")
+#it starts capturing audio
+audiocapture.captureAudio()
+# it will record for 5 seconds
+sleep(5)
+#then it stops recording audio
+audiocapture.stopAudioCapture()
+#it plays audio recorded
+audiocapture.playAudio()
+
+sleep(5)
+
+# setting the recording audio format
+# 8000,11025,16000,22050,44100
+sampleRate = 16000;
+# 8 bit or 16 bit
+sampleSizeInBits = 16;
+# 1 or 2 channels
+channels = 1;
+# bits are signed or unsigned
+bitSigned = True;
+# bigEndian or littleEndian
+bigEndian = False;
+
+# setting audio format     bitrate sample
+audiocapture.setAudioFormat(sampleRate, sampleSizeInBits, channels, bitSigned, bigEndian)
+
 #it starts capturing audio
 audiocapture.captureAudio()
 # it will record for 5 seconds
