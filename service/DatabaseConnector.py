922a0c7e4956215d20f6a61d2ba972ab2b938492
Mats <mats.onnerby@gmail.com>
2017-10-08 23:06:23 +0200

    Update DatabaseConnector.py
    Example script for the DatabaseConnector

--- service/DatabaseConnector.py
+++ service/DatabaseConnector.py
@@ -1,2 +1,21 @@
-# start the service
-databaseconnector = Runtime.start("databaseconnector","DatabaseConnector")
+database = Runtime.start("database","DatabaseConnector")
+database.setDriver("com.mysql.jdbc.Driver")
+database.connectionString="jdbc:mysql://localhost/sakila"
+database.jdbcUser="user"
+database.jdbcPassword="password"
+
+# crawlers publish documents
+def onDocument(doc):
+    print(doc)
+
+database.addListener("publishDocument","python","onDocument")
+
+database.setIdField("actor_id")
+database.setSql("select actor_id, first_name, last_name from actor")
+
+# start crawling
+database.startCrawling()
+
+sleep(5)
+
+database.stopCrawling()
