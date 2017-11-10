f0b45f6cdb26be07d1009f9f1972f34644612b29
GroG <supertick@gmail.com>
2017-11-10 06:18:18 -0800

    Update ps3joymotor.c

--- home/wkinne/ps3joymotor.c
+++ home/wkinne/ps3joymotor.c
@@ -1,1 +1,20 @@
 
+ LY=ps2x.Analog(PSS_LY);
+     analogWrite(enB, LY);
+     Serial.println(LY); 
+     if (LY < 127)  {
+         digitalWrite(in3, LOW);
+         digitalWrite(in4, HIGH);
+         Serial.println(LY); 
+     } 
+     else (LY == 127);  {
+         LY = 0;
+         digitalWrite(in3, HIGH);
+         digitalWrite(in4, LOW);
+         Serial.println(LY);
+     }
+      else (LY > 126);  {
+         digitalWrite(in3, HIGH);
+         digitalWrite(in4, LOW);
+         Serial.println(LY);
+     }
