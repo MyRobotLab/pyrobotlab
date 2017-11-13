27c02dd5aa9b391e1f444cc45777dda60a92012d
moz4r ( Anthony ) <moz4r@free.fr>
2017-11-13 09:59:10 +0100

    AndroidSpeechRecognition.py

--- service/AndroidSpeechRecognition.py
+++ service/AndroidSpeechRecognition.py
@@ -1,2 +1,22 @@
+#########################################
+# AndroidSpeechRecognition.py
+# more info @: http://myrobotlab.org/service/AndroidSpeechRecognition
+# client : https://github.com/moz4r/SpeechRecognitionMRL/blob/master/AndroidSpeechRecognition.apk
+#########################################
+
 # start the service
 androidspeechrecognition = Runtime.start("androidspeechrecognition","AndroidSpeechRecognition")
+
+# start mouth
+marySpeech = Runtime.createAndStart("marySpeech", "MarySpeech")
+
+# auto rearm microphone
+androidspeechrecognition.setAutoListen(True)
+androidspeechrecognition.addCommand("turn on the light", "python", "lightOn")
+androidspeechrecognition.addCommand("turn off the light", "python", "lightOff")
+
+def lightOn():
+  marySpeech.speakBlocking("light is on")
+
+def lightOff():
+  marySpeech.speakBlocking("light is off")
