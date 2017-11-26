73abec53092b73c1603ecfa0aa7d930fb6ea2833
GroG <supertick@gmail.com>
2017-11-26 13:56:35 -0800

    Update Keyboard.py

--- service/Keyboard.py
+++ service/Keyboard.py
@@ -1,12 +1,16 @@
 # create services
 python = Runtime.start("python", "Python")
 keyboard = Runtime.start("keyboard", "Keyboard")
+python.subscribe("keyboard", "publishMouseMoved")
 
 # non blocking event example
 keyboard.addKeyListener(python);
 
 def onKey(key):
-    print "you pressed ", key
+    print ("you pressed ", key)
+
+def onMouseMoved(mouseEvent):
+    print ("x ", mouseEvent.pos.x, " y ", mouseEvent.pos.y)
 
 # blocking example
 print "here waiting"
