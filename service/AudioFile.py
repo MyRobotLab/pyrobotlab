88381ebff47ed097d8576afb8685ac117b3e4a0e
GroG <supertick@gmail.com>
2017-11-30 09:23:23 -0800

    Update AudioFile.py

--- service/AudioFile.py
+++ service/AudioFile.py
@@ -1,2 +1,18 @@
-audiofile = Runtime.createAndStart("audiofile", "AudioFile")
-audiofile.playFile("c:/sounds/beep.mp3")
+#########################################
+# AudioFile.py
+# more info @: http://myrobotlab.org/service/AudioFile
+#########################################
+ 
+# start the services
+audiocapture = Runtime.start("audiocapture","AudioCapture")
+audiofile = Runtime.start("audiofile", "AudioFile")
+
+# it starts capturing audio
+audiocapture.captureAudio()
+# it will record for 5 seconds
+sleep(5)
+# save last capture
+audiocapture.save("mycapture.wav");
+
+# audiofile.playFile("c:/sounds/beep.mp3")
+audiofile.playFile("mycapture.wav")
