24029bf6abb1945c0ba0152284aac12eea4ee58c
Anthony <moz4r@free.fr>
2017-12-04 21:38:59 +0100

    polish mary

--- service/MarySpeech.py
+++ service/MarySpeech.py
@@ -5,21 +5,6 @@ mouth = Runtime.start("MarySpeech", "MarySpeech")
 print ("these are the voices I can have", mouth.getVoices())
 print ("this is the voice I am using", mouth.getVoice())
 
-#set a different voice
-#mouth.setVoice("cmu-slt-hsmm")
-
-# ru voice : download & extract at mrl root : http://www.myai.cloud/mrl/mary-mrl-ru.zip
-#mouth.setVoice("ac-nsh")
-#mouth.speakBlocking(u"привет")
-
-#speak!
-# this blocks until speaking is done
-mouth.speakBlocking(u"Hello world")
-mouth.speakBlocking(u"I speak English. More voices are available, but they need to be installed")
-mouth.speakBlocking(u"Echo echo echo")
-mouth.speakBlocking(u"What should I use")
-mouth.speakBlocking(u"Happy birthday Kyle")
-
 #install a voice:
 #an overview over all official voices is available @ http://myrobotlab.org/service/MarySpeech
 #mouth.installComponentsAcceptLicense(voicename)
@@ -27,10 +12,27 @@ mouth.speakBlocking(u"Happy birthday Kyle")
 #mouth.installComponentsAcceptLicense("bits1")
 
 #switch voice:
-#mouth.setVoice(voicename)
 #mouth.setVoice("bits1")
+#mouth.setVoice("cmu-slt-hsmm")
+#etc...
+
+# ru voice : download & extract at mrl root : http://www.myai.cloud/mrl/mary-mrl-ru.zip
+#mouth.setVoice("ac-nsh")
+#mouth.speakBlocking(u"привет")
+
+#speakBlocking!
+# this blocks until speaking is done
+mouth.speakBlocking(u"Hello world")
+mouth.speakBlocking(u"I speak English. More voices are available, but they need to be installed")
+mouth.speakBlocking(u"Echo echo echo")
+mouth.speakBlocking(u"What should I use")
+
 
 #add voice effects:
 #more effects and information @ http://myrobotlab.org/service/MarySpeech
 mouth.setAudioEffects("FIRFilter+Robot(amount=50)")
 mouth.speakBlocking(u"this is after a sound effect ")
+
+#speak!
+# this not blocks speaking and next line is executed immediatly
+mouth.speak(u"Happy birthday Kyle")
