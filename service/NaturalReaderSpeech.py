2c5d45e964019ac9ed85a05a33ed665f9c03ac32
Anthony <moz4r@free.fr>
2017-12-17 00:35:59 +0100

    unicode voices

--- service/NaturalReaderSpeech.py
+++ service/NaturalReaderSpeech.py
@@ -8,9 +8,12 @@
 
 # start the service
 speech = Runtime.start("Speech", "NaturalReaderSpeech")
-speech.setVoice("Korean_Seoyeon")
+speech.setVoice(u"Korean_Seoyeon")
 speech.speakBlocking(u"내 로봇 연구소는 너무 강력하다")
 #setRate is -x/0/+x
 speech.setRate(-50)
-speech.setVoice("US-English_Ronald")
+speech.setVoice(u"US-English_Ronald")
 speech.speakBlocking(u"Hey, Watson was here!")
+#unicode test
+speech.setVoice(u"French_Chloé")
+speech.speakBlocking(u"coucou les francophones")
