53672df6d7ed06e31627770a7e4fcd9d6afa37a0
moz4r ( Anthony ) <moz4r@free.fr>
2018-05-14 18:41:51 +0200

    Speech.py

--- service/Speech.py
+++ service/Speech.py
@@ -5,5 +5,17 @@
 # more info @: http://myrobotlab.org/service/Speech
 #########################################
 
-# start the service
-speech = Runtime.start('speech','Speech')
+# start all speech services ( to test them )
+
+# local :
+marySpeech = Runtime.start("marySpeech", "MarySpeech")
+localSpeech = Runtime.start("localSpeech", "LocalSpeech")
+mimicSpeech = Runtime.start("mimicSpeech", "MimicSpeech")
+
+# api needed
+polly = Runtime.start("polly", "Polly")
+voiceRss = Runtime.start("voiceRss", "VoiceRss")
+indianTts = Runtime.start("indianTts", "IndianTts")
+
+# beta :
+naturalReaderSpeech = Runtime.start("naturalReaderSpeech", "NaturalReaderSpeech")
