e64c4d6d7f5da445489c948b36d409f8f9e388cd
Langevin Gael <hairygael@gmail.com>
2018-06-24 10:20:05 +0200

    Create InMoov_chatBot.py

---
+++ home/hairygael/InMoov_chatBot.py
@@ -0,0 +1,43 @@
+#########################################
+# Small script to access InMoov chatbot through python script
+#########################################
+Language = "en"
+
+def heard(data):
+  print "Speech Recognition Data:"+str(data)
+
+# create a ProgramAB service and start a session
+Runtime.createAndStart("htmlFilter", "HtmlFilter")
+chatBot=Runtime.start("chatBot", "ProgramAB")
+chatBot.setPath("C:\mrl\myrobotlab.1.0.2693\InMoov\chatbot")
+chatBot.startSession("default",Language)
+
+# Start the WebGui service without starting the browser
+WebGui = Runtime.create("WebGui","WebGui")
+WebGui.autoStartBrowser(False)
+WebGui.startService()
+
+# Then start the browsers and show the WebkitSpeechRecognition service named webkitspeechrecognition
+WebGui.startBrowser("http://localhost:8888/#/service/webkitspeechrecognition")
+ear = Runtime.start("ear","WebkitSpeechRecognition")
+ear.setLanguage("en")
+ear.addListener("publishText", python.name, "heard");
+
+# create a Speech service
+mouth = Runtime.start("mouth", "MarySpeech")
+mouth.installComponentsAcceptLicense("cmu-bdl-hsmm")
+mouth.setVoice("cmu-bdl-hsmm")
+
+# auto rearm microphone
+ear.setAutoListen(True)
+# speedup recognition if False
+ear.setContinuous(False)
+
+######################################################################
+# MRL Routing ear -> program ab -> htmlfilter -> mouth
+######################################################################
+htmlfilter = Runtime.createAndStart("htmlfilter", "HtmlFilter")
+ear.addMouth(mouth)
+ear.addTextListener(chatBot)
+chatBot.addTextListener(htmlfilter)
+htmlfilter.addTextListener(mouth)
