0119e8656787fbe9312aeee410cbcd9af915530d
moz4r ( Anthony ) <moz4r@free.fr>
2018-07-12 15:15:49 +0200

    voicerss nixie

--- service/VoiceRss.py
+++ service/VoiceRss.py
@@ -14,10 +14,11 @@ print ("this is the voice I am using", mouth.getVoice())
 # You sould't not expose keys here, inside gui is a good place
 # But you can do it here ( only once is enough )
 # An AES safe is used to store keys
-# speech.setKeys("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+# speech.setKey("voicerss.api.key","xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
-mouth.setVoice("en-gb")
+mouth.setVoice("Sally")
 
 mouth.speakBlocking("it works, yes I believe it does")
 mouth.setVolume(0.7)
 mouth.speakBlocking("Silent please")
+mouth.setVolume(1)
