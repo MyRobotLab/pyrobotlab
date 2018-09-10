ac2727ef404648333e4e028fd8a158150dad0709
moz4r ( Anthony ) <moz4r@free.fr>
2018-09-10 17:42:39 +0200

    openweathermap update
    https://github.com/MyRobotLab/myrobotlab/issues/255

--- service/OpenWeatherMap.py
+++ service/OpenWeatherMap.py
@@ -1,24 +1,22 @@
-OpenWeatherMap=Runtime.createAndStart("OpenWeatherMap", "OpenWeatherMap")
-OpenWeatherMap.setApiKey("YOUR_KEY") #https://home.openweathermap.org/
-OpenWeatherMap.setUnits("metric") # or imperial
-OpenWeatherMap.setLang("fr") # en / de ...
+owm=Runtime.createAndStart("owm", "OpenWeatherMap")
+owm.setKey("YOUR_KEY") #you can use this only once
+owm.setUnits("metric") # or imperial
+owm.setLang("en") # fr / de ... TODO use locale
 
-# unit is 3 hours steps (1)
+# unit is 3 hours steps (1) , from 1 to 40 ( today to 5 days )
 # fetch raw data for tomorrow (8) -> because 3*8=24H
 # fetch raw data for today (1) -> tomorrow is 1 :
-r=[]
-r=OpenWeatherMap.fetchForecast("paris,fr",1)
-if r:
-  print r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9]
-  # 0:description
-  # 1:temp
-  # 2:location
-  # 3:id // https://github.com/MyRobotLab/inmoov/blob/master/InMoov/chatbot/bots/en/aiml/_inmoovChatbot.aiml#L298
-  # 4:pressure
-  # 5:humidity
-  # 6:temp_min
-  # 7:temp_max
-  # 8:speed
-  # 9:deg
-  # 10:localUnits;
-else:print "error ( api or city )"
+
+owm.setPeriod(1)
+owm.setLocation("Paris,FR")
+
+print "Raw code : ", owm.getWeatherCode()
+print "Town ", owm.getLocation()
+print "The weather is ", owm.getWeatherDescription()
+print owm.getDegrees(), owm.getLocalUnits()
+print "Humidity ", owm.getHumidity()
+print "Min Degrees ", owm.getMinDegrees()
+print "Max Degrees ", owm.getMaxDegrees()
+print "Pressure ", owm.getPressure()
+print "Wind Speed ", owm.getWindSpeed()
+print "Wind Orientation ", owm.getWindOrientation()
