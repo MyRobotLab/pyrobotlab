40fac6dce507be66b864bcc76de347a1df496b7f
Mats Önnerby <mats.onnerby@gmail.com>
2018-11-29 00:48:57 +0100

    Create OpenKinectPointCloud.py
    Example of using BoofCv to create a realtime pointcloud from the Kinect Depth combined with the video stream

---
+++ home/Mats/OpenKinectPointCloud.py
@@ -0,0 +1,9 @@
+#########################################
+# Example of using BoofCv to create a realtime
+# pointcloud from the Kinect Depth combined with
+# the video
+# Navigate using the mouse and keyboard A S D W
+#########################################
+import org.myrobotlab.boofcv.OpenKinectPointCloud as OpenKinectPointCloud
+pk = OpenKinectPointCloud()
+pk.process()
