2524d1bf53596f4461940e24b57fa7afb2823f6a
Langevin Gael <hairygael@gmail.com>
2019-11-07 16:12:59 +0100

    Avoid the eyelid timer when not activated

--- home/hairygael/MoveEyeLidsRandomize.py
+++ home/hairygael/MoveEyeLidsRandomize.py
@@ -5,40 +5,40 @@
 if isEyeLidsActivated:
   MoveEyeLidsTimer = Runtime.start("MoveEyeLidsTimer","Clock")
 
-def MoveEyeLids(timedata):
-  #redefine next loop
-  MoveEyeLidsTimer.setInterval(random.randint(100,10000))
-  if not i01.RobotIsSleeping and not i01.RobotIsTrackingSomething():
-    
-    if isEyeLidsActivated:
-      i01.eyelids.setVelocity(-1,-1)
-      #i01.setEyeLidsVelocity(random.randint(45,-1),random.randint(45,-1),random.randint(45,-1))
-      #move the servo randomly
-      i01.eyelids.moveToBlocking(random.uniform(0,180),random.uniform(0,180))
-      sleep(0.2)
-      i01.eyelids.moveTo(0,0)
-    else:
-      MoveEyeLidsTimer.stopClock()
-  
-#initial function
-def MoveEyeLidsStart():
-  if isEyeLidsActivated:
-    print "moveEyeLidsStart"
+  def MoveEyeLids(timedata):
+    #redefine next loop
+    MoveEyeLidsTimer.setInterval(random.randint(100,10000))
     if not i01.RobotIsSleeping and not i01.RobotIsTrackingSomething():
-      MoveEyeLidsTimer.startClock()
-    else:MoveEyeLidsTimer.stopClock()
+      
+      if isEyeLidsActivated:
+        i01.eyelids.setVelocity(-1,-1)
+        #i01.setEyeLidsVelocity(random.randint(45,-1),random.randint(45,-1),random.randint(45,-1))
+        #move the servo randomly
+        i01.eyelids.moveToBlocking(random.uniform(0,180),random.uniform(0,180))
+        sleep(0.2)
+        i01.eyelids.moveTo(0,0)
+      else:
+        MoveEyeLidsTimer.stopClock()
     
-def MoveEyeLidsStop():
-  
-  if not i01.RobotIsSleeping and not i01.RobotIsTrackingSomething():
+  #initial function
+  def MoveEyeLidsStart():
     if isEyeLidsActivated:
-      MoveEyeLidsTimer.stopClock()
-      i01.eyelids.setVelocity(-1,-1)
-      i01.eyelids.rest()
-
-if RobotCanMoveEyeLids==1:
-  MoveEyeLidsStart()
+      print "moveEyeLidsStart"
+      if not i01.RobotIsSleeping and not i01.RobotIsTrackingSomething():
+        MoveEyeLidsTimer.startClock()
+      else:MoveEyeLidsTimer.stopClock()
+      
+  def MoveEyeLidsStop():
     
-MoveEyeLidsTimer.addListener("pulse", python.name, "MoveEyeLids")
-MoveEyeLidsTimer.addListener("clockStarted", python.name, "MoveEyeLidsStart")
-MoveEyeLidsTimer.addListener("clockStopped", python.name, "MoveEyeLidsStop")
+    if not i01.RobotIsSleeping and not i01.RobotIsTrackingSomething():
+      if isEyeLidsActivated:
+        MoveEyeLidsTimer.stopClock()
+        i01.eyelids.setVelocity(-1,-1)
+        i01.eyelids.rest()
+
+  if RobotCanMoveEyeLids==1:
+    MoveEyeLidsStart()
+      
+  MoveEyeLidsTimer.addListener("pulse", python.name, "MoveEyeLids")
+  MoveEyeLidsTimer.addListener("clockStarted", python.name, "MoveEyeLidsStart")
+  MoveEyeLidsTimer.addListener("clockStopped", python.name, "MoveEyeLidsStop")
